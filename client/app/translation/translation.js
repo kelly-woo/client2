1702889c48bd9a0bfbaa7d9cb856507f75c33b85
tosslab-mk <mk@tosslab.com>
2014-09-03 11:24:44 +0900

    chore(translate): complete on leftpanel translation

--- client/app/translation/translation.js
+++ client/app/translation/translation.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('ko_KR', {"Email address":"이메일 주소","Invalid email or password":"이메일 또는 비밀번호를 확인해주세요","Password":"비밀번호","Sign In":"로그인","Please sign in":"잔디 회원이라면 로그인 하세요"});
+    gettextCatalog.setStrings('ko_KR', {"Channels":"채널","Create channel":"채널 생성","Create private group":"비공개 그룹 생성","Direct Messages":"1:1 메세지","Email address":"이메일 주소","Invalid email or password":"이메일 또는 비밀번호를 확인해주세요","Invite Others":"팀원 초대","Password":"비밀번호","Private Groups":"비공개 그룹","Profile":"프로필 수정","Sign In":"로그인","Sign Out":"로그아웃","more...":"더 보기...","Please sign in":"잔디 회원이라면 로그인 하세요"});
 /* jshint +W100 */
 }]);
