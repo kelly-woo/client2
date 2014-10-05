9934244c46c6cace534c7f93977b5701674292b9
jihoon Kim <jihoonk@tosslab.com>
2014-10-05 21:05:41 +0900

    chore(modal.controller.js): removed unused code.

--- client/app/modal/modal.controller.js
+++ client/app/modal/modal.controller.js
@@ -549,25 +549,6 @@ app.controller('profileCtrl', function($scope, $rootScope, $modalInstance, userA
             $scope.profilePic = e.target.result;
             $scope.croppedProfilePic = '';
             $scope.isProfilePicSelected = true;
-
-//            var modalInstance = $modal.open({
-//                scope       :   $scope,
-//                templateUrl :   'app/modal/image.crop.html',
-//                size        :   'lg',
-//                windowClass :   'wide',
-//                controller  :   'imageCropCtrl',
-//                resolve     : {
-//                    myImage     : function() { return e.target.result; }
-//                }
-//            });
-//
-//            modalInstance.result.then(
-//                function(result) {
-//                    $scope.updateLeftPanelCaller();
-//                },
-//                function() {
-//                    console.log('no chagnes')
-//                });
         };
     };
 
@@ -618,58 +599,6 @@ app.controller('profileCtrl', function($scope, $rootScope, $modalInstance, userA
         $scope.isProfilePicSelected = false;
     };
 
-});
-
-//  IMAGE CROP CONTROLLER
-app.controller('imageCropCtrl', function($scope, $rootScope, $modalInstance, myImage, userAPIservice) {
-    $scope.profilePic = myImage;
-    $scope.croppedProfilePic = '';
-
-    $scope.cancel = function() {
-        $modalInstance.dismiss('cancel');
-    };
-
-    $scope.onCropDone = function() {
-        var blob = dataURItoBlob($scope.croppedProfilePic);
-        $scope.isLoading = true;
-
-        userAPIservice.updateProfilePic(blob)
-            .success(function(response) {
-                $modalInstance.close('success');
-            })
-            .error(function(error) {
-                console.error('onCropDone', error.code, error.msg);
-                $scope.isLoading = false;
-
-            });
-    };
-
-    function dataURItoBlob (dataURI) {
-        // convert base64 to raw binary data held in a string
-        // doesn't handle URLEncoded DataURIs
-        var byteString;
-        if (dataURI.split(',')[0].indexOf('base64') >= 0)
-            byteString = atob(dataURI.split(',')[1]);
-        else
-            byteString = unescape(dataURI.split(',')[1]);
-
-        // separate out the mime component
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-        // write the bytes of the string to an ArrayBuffer
-        var ab = new ArrayBuffer(byteString.length);
-        var ia = new Uint8Array(ab);
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-
-        // write the ArrayBuffer to a blob, and you're done
-        return new Blob([ab],{type: 'image/png'});
-    }
-
-    $scope.onchange = function(temp) {
-        $scope.croppedProfilePic = temp;
-    };
 });
 
 //  PROFILE VIEW CONTROLLER
