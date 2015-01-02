4c4c5233a5b09440d5e0e531bfd8714329ccc6fc
jihoon Kim <jihoonk@tosslab.com>
2015-01-02 19:59:29 +0900

    fix(authSservice): header.authorization.

--- client/app/signin/auth.service.js
+++ client/app/signin/auth.service.js
@@ -126,14 +126,15 @@ app.factory('authInterceptor', function ($rootScope, $q, $window, $injector, con
             // API version
             config.headers.Accept = "application/vnd.tosslab.jandi-v"+$rootScope.api_version+"+json";
 
-            if (config.method === 'POST' && config.fileFormDataName === 'userFile') {
-                // file upload api.
-                if (angular.isUndefined(FileAPI.support)) {
-                // since browser supports html5 file upload feature, FileAPI.support has not been initialized.
-                // console.log('browser supports html5 not including authorization in header.');
-                    return config;
-                }
-            }
+            //if (config.method === 'POST' && config.fileFormDataName === 'userFile') {
+            //    // file upload api.
+            //    if (angular.isUndefined(FileAPI.support)) {
+            //    // since browser supports html5 file upload feature, FileAPI.support has not been initialized.
+            //    // console.log('browser supports html5 not including authorization in header.');
+            //        return config;
+            //    }
+            //}
+
             config.headers.Authorization = (localStorageService.get('token_type') || $window.sessionStorage.token_type) + " " + (storageAPIservice.getAccessToken());
 
             return config;
