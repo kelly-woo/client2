1616f113600fec0f576f88771e883ff33014a866
jihoon Kim <jihoonk@tosslab.com>
2015-01-06 14:40:47 +0900

    refactor(storageAPIservice): applie 'john papa' coding style.

--- client/app/util/service/storage.service.js
+++ client/app/util/service/storage.service.js
@@ -1,8 +1,18 @@
-'use strict';
+(function() {
+    'use strict';
+
+    angular
+        .module('jandiApp')
+        .factory('storageAPIservice', storageAPIservice);
+
+    storageAPIservice.$inject = ['$rootScope', '$window', '$cookieStore', 'localStorageService'];
+    
+    function storageAPIservice($rootScope, $window, $cookieStore, localStorageService) {
+        
+    }
+})();
+
 app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, localStorageService) {
-
-    var storageAPI = {};
-
     var accessToken_key     = 'access_token';
     var refreshToken_key    = 'refresh_token';
     var tokenType_key       = 'token_type';
@@ -18,6 +28,58 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
     var leftDMCollapsed_key = 'is_left_dm_collapsed';
     var leftTopicCollapsed_key = 'is_left_topic_collapsed';
     var leftPGCollapsed_key = 'is_left_pg_collapsed';
+
+    var service = {
+        setTokenLocal: setTokenLocal,
+        setAccessTokenLocal: setAccessTokenLocal,
+        getAccessTokenLocal: getAccessTokenLocal,
+        setRefreshTokenLocal: setRefreshTokenLocal,
+        getRefreshTokenLocal: getRefreshTokenLocal,
+        setTokenTypeLocal: setTokenTypeLocal,
+        getTokenTypeLocal: getTokenTypeLocal,
+        setMemberIdLocal: setMemberIdLocal,
+        getMemberIdLocal: getMemberIdLocal,
+        setLastEmail: setLastEmail,
+        getLastEmail: getLastEmail,
+        setLastLang: setLastLang,
+        getLastLang: getLastLang,
+        hasAccessTokenLocal: hasAccessTokenLocal,
+        setLastStateLocal: setLastStateLocal,
+        getLastStateLocal: getLastStateLocal,
+        removeLastStateLocal: removeLastStateLocal,
+        setAccountInfoLocal: setAccountInfoLocal,
+        getAccountIdLocal: getAccountIdLocal,
+        getTeamIdLocal: getTeamIdLocal,
+        getMemberIdLocal: getMemberIdLocal,
+        removeLocal: removeLocal,
+        setLeftTopicCollapsed: setLeftTopicCollapsed,
+        isLeftTopicCollapsed: isLeftTopicCollapsed,
+        setLeftPGCollapsed: setLeftPGCollapsed,
+        isLeftPGCollapsed: isLeftPGCollapsed,
+        setLeftDMCollapsed: setLeftDMCollapsed,
+        isLeftDMCollapsed: isLeftDMCollapsed,
+        setLeftListStatus: setLeftListStatus,
+        setTokenSession: setTokenSession,
+        setAccessTokenSession: setAccessTokenSession,
+        getAccessTokenSession: getAccessTokenSession,
+        setRefreshTokenSession: setRefreshTokenSession,
+        getRefreshTokenSession: getRefreshTokenSession,
+        setTokenTypeSession: setTokenTypeSession,
+        getTokenTypeSession: getTokenTypeSession,
+        hasAccessTokenSession: hasAccessTokenSession,
+        setAcountInfoSession: setAcountInfoSession,
+        getAccountIdSession: getAccountIdSession,
+        getTeamIdSession: getTeamIdSession,
+        getMemberIdSession: getMemberIdSession,
+        removeSession: removeSession,
+        setAccessTokenLocalCookie: setAccessTokenLocalCookie,
+        isValidValue: isValidValue,
+        getAccessToken: getAccessToken,
+        getRefreshToken: getRefreshToken
+    };
+
+    return service;
+
     /**
      *
      * localStorage Code below.
@@ -27,43 +89,43 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
      *
      */
 
-    //////////////////////////////////////////
-    //
-    //            token
-    //
-    ///////////////////////////////
-    storageAPI.setTokenLocal = function(tokenData) {
-        this.setAccessTokenLocal(tokenData.access_token);
-        this.setRefreshTokenLocal(tokenData.refresh_token);
-        this.setTokenTypeLocal(tokenData.token_type);
+        //////////////////////////////////////////
+        //
+        //            token
+        //
+        ///////////////////////////////
+    function setTokenLocal (tokenData) {
+        setAccessTokenLocal(tokenData.access_token);
+        setRefreshTokenLocal(tokenData.refresh_token);
+        setTokenTypeLocal(tokenData.token_type);
 
-    };
+    }
 
     // access_token getter & setter
-    storageAPI.setAccessTokenLocal = function(access_token) { localStorageService.set(accessToken_key, access_token); };
-    storageAPI.getAccessTokenLocal = function() { return localStorageService.get(accessToken_key); };
+    function setAccessTokenLocal (access_token) { localStorageService.set(accessToken_key, access_token); }
+    function getAccessTokenLocal () { return localStorageService.get(accessToken_key); }
 
     // refresh_token getter & setter
-    storageAPI.setRefreshTokenLocal = function(refresh_token) { localStorageService.set(refreshToken_key, refresh_token); };
-    storageAPI.getRefreshTokenLocal = function() { return localStorageService.get(refreshToken_key); };
+    function setRefreshTokenLocal (refresh_token) { localStorageService.set(refreshToken_key, refresh_token); }
+    function getRefreshTokenLocal () { return localStorageService.get(refreshToken_key); }
 
     // token_type getter & setter
-    storageAPI.setTokenTypeLocal = function(token_type) { localStorageService.set(tokenType_key, token_type); };
-    storageAPI.getTokenTypeLocal = function() { return localStorageService.get(tokenType_key); };
+    function setTokenTypeLocal (token_type) { localStorageService.set(tokenType_key, token_type); }
+    function getTokenTypeLocal () { return localStorageService.get(tokenType_key); }
 
     // token_type getter & setter
-    storageAPI.setMemberIdLocal = function(token_type) { localStorageService.set(tokenType_key, token_type); };
-    storageAPI.getMemberIdLocal = function() { return localStorageService.get(tokenType_key); };
+    function setMemberIdLocal (token_type) { localStorageService.set(tokenType_key, token_type); }
+    function getMemberIdLocal () { return localStorageService.get(tokenType_key); }
 
-    storageAPI.setLastEmail = function(email) { localStorageService.set(lastEmail_key, email); };
-    storageAPI.getLastEmail = function() { return localStorageService.get(lastEmail_key); };
+    function setLastEmail (email) { localStorageService.set(lastEmail_key, email); }
+    function getLastEmail () { return localStorageService.get(lastEmail_key); }
 
-    storageAPI.setLastLang = function(lang) { localStorageService.set(lastLang_key, lang); };
-    storageAPI.getLastLang = function() { return localStorageService.get(lastLang_key); };
+    function setLastLang (lang) { localStorageService.set(lastLang_key, lang); }
+    function getLastLang () { return localStorageService.get(lastLang_key); }
 
-    storageAPI.hasAccessTokenLocal = function() {
-        return this.getAccessTokenLocal();
-    };
+    function hasAccessTokenLocal () {
+        return getAccessTokenLocal();
+    }
 
 
     //////////////////////////////////////////
@@ -71,41 +133,41 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
     //            Last State
     //
     ///////////////////////////////
-    storageAPI.setLastStateLocal = function(last_state) {
+    function setLastStateLocal (last_state) {
         localStorageService.set(lastState_key, last_state);
-    };
-    storageAPI.getLastStateLocal = function() { localStorageService.get(lastState_key); };
+    }
+    function getLastStateLocal () { localStorageService.get(lastState_key); }
 
-    storageAPI.removeLastStateLocal = function() { localStorageService.remove(lastState_key); };
+    function removeLastStateLocal () { localStorageService.remove(lastState_key); }
 
     //////////////////////////////////////////
     //
     //            Account Info
     //
     ///////////////////////////////
-    storageAPI.setAccountInfoLocal = function(accountId, teamId, memberId) {
+    function setAccountInfoLocal (accountId, teamId, memberId) {
         localStorageService.set(accountId_key, accountId);
         localStorageService.set(teamId_key, teamId);
         localStorageService.set(memberId_key, memberId);
-    };
+    }
 
-    storageAPI.getAccountIdLocal = function() { return localStorageService.get(accountId_key); };
-    storageAPI.getTeamIdLocal = function() { return localStorageService.get(teamId_key); };
-    storageAPI.getMemberIdLocal = function() { return localStorageService.get(memberId_key); };
+    function getAccountIdLocal () { return localStorageService.get(accountId_key); }
+    function getTeamIdLocal () { return localStorageService.get(teamId_key); }
+    function getMemberIdLocal () { return localStorageService.get(memberId_key); }
 
 
     /*
-        Removes
-            access_token
-            refresh_token
-            account_id
-            team_id
-            member_id
-        in localStorage.
+     Removes
+     access_token
+     refresh_token
+     account_id
+     team_id
+     member_id
+     in localStorage.
 
-        Basically, everything except last_state.
-    */
-    storageAPI.removeLocal = function() {
+     Basically, everything except last_state.
+     */
+    function removeLocal () {
         localStorageService.remove(accessToken_key);
         localStorageService.remove(refreshToken_key);
         localStorageService.remove(tokenType_key);
@@ -116,24 +178,24 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
         localStorageService.remove(leftPGCollapsed_key);
         localStorageService.remove(leftDMCollapsed_key);
 
-    };
+    }
 
 
     // TODO: to be implemented.
-    storageAPI.setLeftTopicCollapsed = function(isCollapsed) { localStorageService.set(leftTopicCollapsed_key, isCollapsed); };
-    storageAPI.isLeftTopicCollapsed = function() { return localStorageService.get(leftTopicCollapsed_key) === 'true'; };
+    function setLeftTopicCollapsed (isCollapsed) { localStorageService.set(leftTopicCollapsed_key, isCollapsed); }
+    function isLeftTopicCollapsed () { return localStorageService.get(leftTopicCollapsed_key) === 'true'; }
 
-    storageAPI.setLeftPGCollapsed = function(isCollapsed) { localStorageService.set(leftPGCollapsed_key, isCollapsed); };
-    storageAPI.isLeftPGCollapsed = function() { return localStorageService.get(leftPGCollapsed_key) === 'true';;   };
+    function setLeftPGCollapsed (isCollapsed) { localStorageService.set(leftPGCollapsed_key, isCollapsed); }
+    function isLeftPGCollapsed () { return localStorageService.get(leftPGCollapsed_key) === 'true';;   }
 
-    storageAPI.setLeftDMCollapsed = function(isCollapsed) { localStorageService.set(leftDMCollapsed_key, isCollapsed); };
-    storageAPI.isLeftDMCollapsed = function() { return localStorageService.get(leftDMCollapsed_key) === 'true';; };
+    function setLeftDMCollapsed (isCollapsed) { localStorageService.set(leftDMCollapsed_key, isCollapsed); }
+    function isLeftDMCollapsed () { return localStorageService.get(leftDMCollapsed_key) === 'true';; }
 
-    storageAPI.setLeftListStatus = function(status) {
-        storageAPI.setLeftTopicCollapsed(status.isTopicCollapsed);
-        storageAPI.setLeftPGCollapsed(status.isPGCollapsed);
-        storageAPI.setLeftDMCollapsed(status.isDMCollapsed);
-    };
+    function setLeftListStatus (status) {
+        setLeftTopicCollapsed(status.isTopicCollapsed);
+        setLeftPGCollapsed(status.isPGCollapsed);
+        setLeftDMCollapsed(status.isDMCollapsed);
+    }
 
     /**
      *
@@ -150,54 +212,53 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
      *
      */
 
-    //////////////////////////////////////////
-    //
-    //            token
-    //
-    ///////////////////////////////
-    storageAPI.setTokenSession = function(tokenData) {
-        this.setAccessTokenSession(tokenData.access_token);
-        this.setRefreshTokenSession(tokenData.refresh_token);
-        this.setTokenTypeSession(tokenData.token_type);
-    };
+        //////////////////////////////////////////
+        //
+        //            token
+        //
+        ///////////////////////////////
+    function setTokenSession (tokenData) {
+        setAccessTokenSession(tokenData.access_token);
+        setRefreshTokenSession(tokenData.refresh_token);
+        setTokenTypeSession(tokenData.token_type);
+    }
 
-    storageAPI.setAccessTokenSession = function(access_token) { $window.sessionStorage.access_token = access_token; };
-    storageAPI.getAccessTokenSession = function() { return $window.sessionStorage.access_token; };
+    function setAccessTokenSession (access_token) { $window.sessionStorage.access_token = access_token; }
+    function getAccessTokenSession () { return $window.sessionStorage.access_token; }
 
-    storageAPI.setRefreshTokenSession = function(refresh_token) { $window.sessionStorage.refresh_token = refresh_token; };
-    storageAPI.getRefreshTokenSession = function() { return $window.sessionStorage.refresh_token; };
+    function setRefreshTokenSession (refresh_token) { $window.sessionStorage.refresh_token = refresh_token; }
+    function getRefreshTokenSession () { return $window.sessionStorage.refresh_token; }
 
-    storageAPI.setTokenTypeSession = function(token_type) { $window.sessionStorage.token_type = token_type; };
-    storageAPI.getTokenTypeSession = function() { return $window.sessionStorage.token_type; };
+    function setTokenTypeSession (token_type) { $window.sessionStorage.token_type = token_type; }
+    function getTokenTypeSession () { return $window.sessionStorage.token_type; }
 
     // checking window session storage.
-    storageAPI.hasAccessTokenSession = function() { return this.getAccessTokenSession(); };
-
+    function hasAccessTokenSession () { return getAccessTokenSession(); }
 
     //////////////////////////////////////////
     //
     //            Account Info
     //
     ///////////////////////////////
-    storageAPI.setAcountInfoSession =function(accountId, teamId, memberId) {
+    function setAcountInfoSession (accountId, teamId, memberId) {
         $window.sessionStorage.account_id   = accountId;
         $window.sessionStorage.team_id      = teamId;
         $window.sessionStorage.member_id    = memberId;
-    };
+    }
 
-    storageAPI.getAccountIdSession = function() { return $window.sessionStorage.account_id; };
-    storageAPI.getTeamIdSession = function() { return $window.sessionStorage.team_id; };
-    storageAPI.getMemberIdSession = function() { return $window.sessionStorage.member_id; };
+    function getAccountIdSession () { return $window.sessionStorage.account_id; }
+    function getTeamIdSession () { return $window.sessionStorage.team_id; }
+    function getMemberIdSession () { return $window.sessionStorage.member_id; }
 
     // Removes everything in $window.sessionStorage.
-    storageAPI.removeSession = function() {
+    function removeSession () {
         delete $window.sessionStorage.access_token;
         delete $window.sessionStorage.refresh_token;
         delete $window.sessionStorage.account_id;
         delete $window.sessionStorage.team_id;
         delete $window.sessionStorage.member_id;
         $window.sessionStorage.clear();
-    };
+    }
 
 
     /**
@@ -205,35 +266,34 @@ app.factory('storageAPIservice', function($rootScope, $window, $cookieStore, loc
      * @param tokenData
      */
 
-    storageAPI.setAccessTokenLocalCookie = function(tokenData) {
+    function setAccessTokenLocalCookie (tokenData) {
         $cookieStore.put(accessToken_key, tokenData.access_token);
-    };
+    }
 
 
-    storageAPI.isValidValue = function(input) {
+    function isValidValue (input) {
         if(angular.isUndefined(input) || input === null || input == 'undefined') return false;
         return true;
-    };
+    }
 
-    storageAPI.getAccessToken = function() {
-        if (storageAPI.isValidValue(storageAPI.getAccessTokenLocal()))
-            return storageAPI.getAccessTokenLocal();
-        if (storageAPI.isValidValue(storageAPI.getAccessTokenSession()))
-            return storageAPI.getAccessTokenSession();
-
-        return false;
-    };
-
-    storageAPI.getRefreshToken = function() {
-        if (storageAPI.isValidValue(storageAPI.getRefreshTokenLocal()))
-            return storageAPI.getRefreshTokenLocal();
-        if (storageAPI.isValidValue(storageAPI.getRefreshTokenSession()))
-            return storageAPI.getRefreshTokenSession();
+    function getAccessToken () {
+        if (isValidValue(getAccessTokenLocal()))
+            return getAccessTokenLocal();
+        if (isValidValue(getAccessTokenSession()))
+            return getAccessTokenSession();
 
         return false;
-    };
+    }
 
-    return storageAPI;
+    function getRefreshToken () {
+        if (isValidValue(getRefreshTokenLocal()))
+            return getRefreshTokenLocal();
+        if (isValidValue(getRefreshTokenSession()))
+            return getRefreshTokenSession();
+
+        return false;
+    }
+
 });
 
 
