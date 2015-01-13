72f101af6d006c2f299b92de5f4dedca61c86cf7
jihoon Kim <jihoonk@tosslab.com>
2015-01-14 00:27:33 +0900

    fix(desktopNotificationService): refactored.

--- client/app/util/service/desktop.notification.service.js
+++ client/app/util/service/desktop.notification.service.js
@@ -9,73 +9,58 @@
 
     function desktopNotificationService($rootScope, $state, $filter, entityAPIservice, accountService) {
         var service = {
-            notify: notify,
             addNotification: addNotification
         };
 
         return service;
 
+        function addNotification(fromEntity, toEntity) {
 
-        function notify(title, notificationOption) {
+            var notificationOption = getNotificationOption(fromEntity, toEntity);
+
+            // Set title for notification.
+            var title = $filter('translate')('@web-notification-title');
+
             if (!("Notification" in window)) {
                 // Notification not supported by Browser.
                 return;
             }
             else if (Notification.permission === "granted") {
-
-                // If it's okay let's create a notification.
-                var notification = new Notification(title, notificationOption);
-
-                // Decides what it does when notification pops up.
-                notification.onshow = function() {
-                    notificationFadeOut(notification);
-                };
-
-                // Decides what to do when notification click.
-                notification.onclick = function() {
-                    onNotificationClicked(notification);
-                };
+                notify(title, notificationOption);
             }
-
-            // Otherwise, we need to ask the user for permission
-            // Note, Chrome does not implement the permission static property
-            // So we have to check for NOT 'denied' instead of 'default'
             else if (Notification.permission !== 'denied') {
+                // Otherwise, we need to ask the user for permission
+                // Note, Chrome does not implement the permission static property
+                // So we have to check for NOT 'denied' instead of 'default'
                 Notification.requestPermission(function (permission) {
                     // If the user is okay, let's create a notification
                     if (permission === "granted") {
-                        var notification = new Notification("Hi there!");
+                        notify(title, notificationOption);
                     }
                 });
             }
-
-            // At last, if the user already denied any notification, and you
-            // want to be respectful there is no need to bother them any more.
         }
 
-
-        function addNotification(fromEntity, toEntity) {
-
+        function getNotificationOption(fromEntity, toEntity) {
+            // Start with entity
             var notificationOption = toEntity;
 
+            // Get bodyMessage for notification.
             var bodyMessage = getNotificationBodyMessage(fromEntity, toEntity);
 
+            // Set appropriate attributes to notification.
             notificationOption.tag = toEntity.id;
             notificationOption.body= bodyMessage;
             notificationOption.icon= $filter('getSmallThumbnail')(fromEntity);
 
-            var title = $filter('translate')('@web-notification-title');
-
-            notify(title, notificationOption);
+            return notificationOption;
         }
-
         function getNotificationBodyMessage(fromEntity, toEntity) {
             if (toEntity.type == 'user')
                 return generateMessagesNotificationBody(fromEntity);
 
             return generateTopicNotificationBody(fromEntity, toEntity)
         }
-
         // Generates body message for 1:1 messages notification.
         function generateMessagesNotificationBody(fromEntity) {
             return $filter('translate')('@web-notification-body-messages-pre')
@@ -94,23 +79,23 @@
             switch (currentLanguage) {
                 case 'ko' :
                     bodyMessage = '[' + toEntity.name + ']' + $filter('translate')('@web-notification-body-topic-mid')
-                                    + fromEntity.name;
+                    + fromEntity.name;
                     break;
                 case 'ja' :
                     bodyMessage = fromEntity.name + $filter('translate')('@web-notification-body-topic-mid')
-                                    + '[' + toEntity.name + ']';
+                    + '[' + toEntity.name + ']';
                     break;
                 case 'en' :
                     bodyMessage = $filter('translate')('@web-notification-body-topic-pre')
-                                    + fromEntity.name
-                                    + $filter('translate')('@web-notification-body-topic-mid')
-                                    + '[' + toEntity.name + ']';
+                    + fromEntity.name
+                    + $filter('translate')('@web-notification-body-topic-mid')
+                    + '[' + toEntity.name + ']';
                     break;
                 default :
                     bodyMessage = $filter('translate')('@web-notification-body-topic-pre')
-                                    + '[' + toEntity.name + ']'
-                                    + $filter('translate')('@web-notification-body-topic-mid')
-                                    + fromEntity.name;
+                    + '[' + toEntity.name + ']'
+                    + $filter('translate')('@web-notification-body-topic-mid')
+                    + fromEntity.name;
                     break;
             }
 
@@ -124,11 +109,25 @@
                 + $filter('translate')('@web-notification-body-topic-post');
         }
 
+        function notify(title, notificationOption) {
+            // An actually notification object.
+            var notification = new Notification(title, notificationOption);
+
+            // Decides what it does when notification pops up.
+            notification.onshow = function() {
+                notificationFadeOut(notification);
+            };
+
+            // Decides what to do when notification click.
+            notification.onclick = function() {
+                onNotificationClicked(notification);
+            };
+        }
+
         // Binds fadeout effect on notification.
         function notificationFadeOut(noti) {
             setTimeout(noti.close.bind(noti), 1500000);
         }
-
         // Takes user to topic/messages where notification came from.
         function onNotificationClicked(noti) {
             var toEntity = entityAPIservice.getEntityFromListById($rootScope.totalEntities, noti.tag);
