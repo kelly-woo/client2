3a2a88d77b0431a2fde6989cedd5df07029350df
jihoon Kim <jihoonk@tosslab.com>
2015-01-26 18:32:35 +0900

    fix(fileicon): Apply file icon for .hwp type.

--- client/app/right/file.filter.js
+++ client/app/right/file.filter.js
@@ -3,98 +3,176 @@
 var app = angular.module('jandiApp');
 
 app.filter('fileByTitle', function() {
-    return function(input, title) {
-        if (title === undefined) {
-            return input;
-        }
-        title = title.toLowerCase();
-
-        var return_array = [];
-        angular.forEach(input, function(file, index) {
-            var fileTitle = file.content.title.toLowerCase();
-
-            if (fileTitle.indexOf(title) > -1 )
-                this.push(file)
-        }, return_array);
-
-        return return_array;
+  return function(input, title) {
+    if (title === undefined) {
+      return input;
     }
+    title = title.toLowerCase();
+
+    var return_array = [];
+    angular.forEach(input, function(file, index) {
+      var fileTitle = file.content.title.toLowerCase();
+
+      if (fileTitle.indexOf(title) > -1 )
+        this.push(file)
+    }, return_array);
+
+    return return_array;
+  }
 });
 
 app.filter('fileByWriterId', function() {
-    return function(input, id) {
-        if (id === undefined || id === 'everyone') {
-            return input;
-        }
-
-        var return_array = [];
-        angular.forEach(input, function(file, index) {
-            var fileWriterId = file.writerId;
-
-            if (fileWriterId == id ) {
-                this.push(file)
-            }
-        }, return_array);
-
-        return return_array;
+  return function(input, id) {
+    if (id === undefined || id === 'everyone') {
+      return input;
     }
+
+    var return_array = [];
+    angular.forEach(input, function(file, index) {
+      var fileWriterId = file.writerId;
+
+      if (fileWriterId == id ) {
+        this.push(file)
+      }
+    }, return_array);
+
+    return return_array;
+  }
 });
 
 app.filter('fileByType', function() {
-    return function (input, type) {
+  return function (input, type) {
 
-        if (angular.isUndefined(type) || type == 'All') {
-            return input;
-        }
-
-        type = type.toLowerCase();
-
-        var return_array = [];
-        angular.forEach(input, function (file, index) {
-            var fileType = file.content.type;
-
-            if (fileType.indexOf(type) > -1) {
-                this.push(file)
-            }
-        }, return_array);
-
-        return return_array;
+    if (angular.isUndefined(type) || type == 'All') {
+      return input;
     }
+
+    type = type.toLowerCase();
+
+    var return_array = [];
+    angular.forEach(input, function (file, index) {
+      var fileType = file.content.type;
+
+      if (fileType.indexOf(type) > -1) {
+        this.push(file)
+      }
+    }, return_array);
+
+    return return_array;
+  }
 });
 
 /*
  * @filter      : file type(extension) formatting
  */
 app.filter('filetype', function() {
-    return function(type) {
-        if (typeof type === 'undefined') return 'undefined';
-        var filetype = "";
-        switch(type) {
-            case 'image/jpeg'       :   filetype = "JPG"; break;
-            case 'image/png'        :   filetype = "PNG"; break;
-            case 'image/gif'        :   filetype = "GIF"; break;
-            case 'application/pdf'  :   filetype = "PDF"; break;
-            case 'video/mp4'        :   filetype = "MP4"; break;
-            case 'video/quicktime'  :   filetype = "MOV"; break;
-            case 'audio/mp3'        :   filetype = "MP3"; break;
-            case 'audio/mpeg'       :   filetype = "MPEG"; break;
-            case 'application/zip'  :   filetype = "ZIP"; break;
+  return function(type) {
+    if (typeof type === 'undefined') return 'undefined';
 
-            case 'text/plain'           :
-            case 'application/msword'   :    // doc
-            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :    // docx
-                                        filetype = "TXT"; break;
+    var filetype = "";
 
-            case 'application/vnd.ms-excel' :          // xls
-            case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :          // xlsx
-                                        filetype = "EXCEL"; break;
+    switch(type) {
+      case 'image/jpeg'       :   filetype = "JPG"; break;
 
-            case 'application/vnd.ms-powerpoint' :      // ppt
-            case 'application/vnd.openxmlformats-officedocument.presentationml.presentation' :  // pptx
-                                        filetype = 'PPT'; break;
+      case 'image/png'        :   filetype = "PNG"; break;
 
-            default                 :   filetype = "ETC"; break;
-        }
-        return filetype;
-    };
+      case 'image/gif'        :   filetype = "GIF"; break;
+
+      case 'application/pdf'  :   filetype = "PDF"; break;
+
+      case 'video/mp4'        :   filetype = "MP4"; break;
+
+      case 'video/quicktime'          :   filetype = "MOV"; break;
+      case 'application/octet-stream' :   filetype = "MOV"; break;
+
+      case 'video/x-matroska' : filetype = "MKV"; break; // mkv
+
+      case 'audio/mp3'        :   filetype = "MP3"; break;
+
+      case 'audio/mpeg'       :   filetype = "MPEG"; break;
+
+      case 'application/zip'  :   filetype = "ZIP"; break;
+
+      case 'application/x-hwp': filetype = "HWP"; break;
+
+      case 'text/plain'           :
+      case 'application/msword'   :    // doc
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :    // docx
+        filetype = "TXT"; break;
+
+      case 'application/vnd.ms-excel' :          // xls
+      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :          // xlsx
+        filetype = "EXCEL"; break;
+
+      case 'application/vnd.ms-powerpoint' :      // ppt
+      case 'application/vnd.openxmlformats-officedocument.presentationml.presentation' :  // pptx
+        filetype = 'PPT'; break;
+
+      default                 :   filetype = "ETC"; break;
+    }
+    return filetype;
+  };
+});
+
+/**
+ *
+ */
+app.filter('getFileIconImage', function() {
+  return function(type) {
+    if (typeof type === 'undefined') return 'undefined';
+
+    var filetype = "";
+
+    switch(type) {
+      // Image
+      case 'image/jpeg' : filetype = "img"; break;
+      case 'image/png'  : filetype = "img"; break;
+      case 'image/gif'  : filetype = "img"; break;
+
+      // PDF
+      case 'application/pdf'  : filetype = "pdf"; break;
+
+      // Video
+      case 'video/mp4'                :
+      case 'video/quicktime'          :
+      case 'application/octet-stream' :
+      case 'video/x-matroska' : // mkv
+        filetype = "video";
+        break;
+
+      // Audio
+      case 'audio/mp3'  : filetype = "audio"; break;
+      case 'audio/mpeg' : filetype = "audio"; break;
+
+      // Zip
+      case 'application/zip'  : filetype = "zip"; break;
+
+      // Hanguel
+      case 'application/x-hwp': filetype = "hwp"; break;
+
+      // Text
+      case 'text/plain'           :
+      case 'application/msword'   :    // doc
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :    // docx
+        filetype = "TXT";
+        break;
+
+      // Excel
+      case 'application/vnd.ms-excel' :          // xls
+      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :          // xlsx
+        filetype = "EXCEL";
+        break;
+
+      // Etc
+      case 'application/vnd.ms-powerpoint' :      // ppt
+      case 'application/vnd.openxmlformats-officedocument.presentationml.presentation' :  // pptx
+        filetype = 'PPT';
+        break;
+
+      default:
+        filetype = "ETC";
+        break;
+    }
+    return filetype;
+  };
 });
