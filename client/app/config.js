2a7e071b0072238ae7841cbb959e2a01c76981b3
jihoon Kim <jihoonk@tosslab.com>
2015-01-30 14:46:57 +0900

    fix(config): Change port from 8888 to 4000 for archived file testing.

--- client/app/config.js
+++ client/app/config.js
@@ -3,7 +3,7 @@
 angular.module('services.config', [])
   .constant('configuration', {
     name                : 'local',
-    api_address         : 'http://www.jandi.io:8888/',
+    api_address         : 'http://www.jandi.io:4000/',
     api_version         : '2',
     ga_token            : 'UA-54051037-1',
     ga_token_global     : 'UA-54051037-4',
