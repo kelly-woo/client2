5d257dfdf8a62142336a5ec0b1ea481e13629764
jihoon Kim <jihoonk@tosslab.com>
2015-02-17 22:25:43 +0900

    fix(urlSCheme): Refactor URLSCHEME to its own service.

--- client/app/mobile/mobile.catcher.controller.js
+++ client/app/mobile/mobile.catcher.controller.js
@@ -3,7 +3,7 @@
         .module('jandiApp')
         .controller('mobileCatcherController', mobileCatcherController);
 
-    function mobileCatcherController($scope, $state, $rootScope, configuration, publicService, storageAPIservice) {
+    function mobileCatcherController($scope, $state, $rootScope, urlScheme) {
 
         (function() {
             if(angular.isUndefined($rootScope.mobileStatus))
@@ -18,39 +18,8 @@
         })();
 
         $scope.toMobileApplication = function() {
-            var url_to_app, url_to_store;
-
-            var access_token = $rootScope.mobileStatus.access_token;
-            if (!access_token) access_token = '';
-            var refresh_token = $rootScope.mobileStatus.refresh_token;
-            if (!refresh_token) refresh_token = '';
-
-            url_to_app = 'tosslabjandi://open?access_token=' + access_token + '&refresh_token=' + refresh_token;
-
-            if ($scope.isAndroid) {
-                url_to_store = configuration.play_store_address;
-            }
-            else {
-                url_to_store = configuration.app_store_address;
-            }
-            startApp(url_to_app, url_to_store);
+          urlScheme.urlScheme();
         };
-
-        var timeout;
-        function preventPopup() {
-            clearTimeout(timeout);
-            timeout = null;
-            window.removeEventListener('pagehide', preventPopup);
-        }
-
-        function startApp(url_to_app, url_to_store) {
-            publicService.redirectTo(url_to_app);
-            timeout = setTimeout(function(){
-                publicService.redirectTo(url_to_store);
-            }, 1000);
-            window.addEventListener('pagehide', preventPopup);
-        }
-
 
         $scope.isLang = function(lang) {
             return $rootScope.language == lang;
---
+++ client/app/util/service/urlScheme.service.js
@@ -0,0 +1,111 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('jandiApp')
+    .service('urlScheme', urlScheme);
+
+  /* @ngInject */
+  function urlScheme(storageAPIservice, $rootScope, configuration) {
+    this.urlScheme = urlScheme;
+
+    function redirectTo(url) {
+      // Direct to 'url'.
+      location.href = url;
+    }
+
+    /**
+     * Send url scheme.
+     */
+    function urlScheme() {
+      var url_to_app, url_to_store;
+
+      if (_isAndroid())
+        url_to_store = configuration.play_store_address;
+      else
+        url_to_store = configuration.app_store_address;
+
+      var access_token = _getAccessToken();
+      var refresh_token = _getRefreshToken();
+
+      url_to_app = 'tosslabjandi://open?' + access_token + '' + refresh_token;
+
+      _startApp(url_to_app, url_to_store);
+    }
+
+    /**
+     *
+     * @returns {boolean}
+     *  true, if user is using android device.
+     *  false, if user is using non-android device.
+     *
+     *  @private
+     */
+    function _isAndroid() {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      return userAgent.match(/Android/i)=='Android';
+    }
+
+    /**
+     *
+     * @returns {access_token with correct format 'access_token=token_value}
+     * @private
+     */
+    function _getAccessToken() {
+      var access_token = storageAPIservice.getAccessToken() || $rootScope.mobileStatus.access_token;
+      if (!!access_token) {
+        access_token = 'access_token='+access_token;
+      }
+      else {
+        access_token = '';
+      }
+      return access_token;
+    }
+
+    /**
+     *
+     * @returns {refresh_token with correct format '&refresh_token=token_value}
+     * @private
+     */
+    function _getRefreshToken() {
+      var refresh_token = storageAPIservice.getRefreshToken() || $rootScope.mobileStatus.refresh_token;
+      if (!!refresh_token) {
+        refresh_token = '&refresh_token='+refresh_token;
+      }
+      else {
+        refresh_token = '';
+      }
+      return refresh_token;
+    }
+
+    var timeout;
+
+
+    /**
+     *
+     * @param url_to_app: url to application.
+     * @param url_to_store: url to store.
+     * @private
+     */
+    function _startApp(url_to_app, url_to_store) {
+      redirectTo(url_to_app);
+
+      window.addEventListener('pagehide', _preventPopup);
+
+      timeout = setTimeout(function(){
+        redirectTo(url_to_store);
+      }, 1500);
+
+    }
+    /**
+     * Prevent alert window from popping up.
+     */
+    function _preventPopup() {
+      clearTimeout(timeout);
+      timeout = null;
+      window.removeEventListener('pagehide', _preventPopup);
+    }
+
+  }
+
+})();
