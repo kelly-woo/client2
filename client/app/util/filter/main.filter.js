8ea0e0cbcbf81d9a3d798978033ed32a165c7ea6
Mark <mark.park@tosslab.com>
2015-04-08 11:52:20 +0900

    fix(center): Fix parseUrl filter.   - add parseAnchor filter

--- client/app/util/filter/main.filter.js
+++ client/app/util/filter/main.filter.js
@@ -33,6 +33,110 @@ app.filter('bytes', function() {
   }
 });
 
+// Url, Email 식별 정규식
+// http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.0.2_r1/android/util/Patterns.java
+// 참조함
+app.filter('parseAnchor', function() {
+  var DEFAULT_PROTO,
+      GOOD_IRI_CHAR,
+      IRI,
+      GOOD_GTLD_CHAR,
+      GTLD,
+      HOST_NAME,
+      IP_ADDRESS,
+      DOMAIN_NAME,
+      rWebUrl,
+      rEmailAddr,
+      rSplit,
+      escapeMap;
+
+  DEFAULT_PROTO = 'http://';
+
+  GOOD_IRI_CHAR = 'a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF';
+  IRI = '[' + GOOD_IRI_CHAR + ']([' + GOOD_IRI_CHAR + '\\-]{0,61}[' + GOOD_IRI_CHAR + ']){0,1}';
+  GOOD_GTLD_CHAR = 'a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF';
+  GTLD = '[' + GOOD_GTLD_CHAR + ']{2,63}';
+  HOST_NAME = '(' + IRI + '\\.)+' + GTLD;
+  IP_ADDRESS = '((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9]))';
+  DOMAIN_NAME = '(' + HOST_NAME + '|' + IP_ADDRESS + ')';
+
+  rWebUrl = new RegExp(
+    '((?:(http|https|Http|Https|rtsp|Rtsp):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\\'\\(\\)' +
+    '\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_' +
+    '\\.\\+\\!\\*\\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?' +
+    '(?:' + DOMAIN_NAME + ')' +
+    '(?:\\:\\d{1,5})?)' +                                               // plus option port number
+    '(\\/(?:(?:[' + GOOD_IRI_CHAR + '\\;\\/\\?\\:\\@\\&\\=\\#\\~' +     // plus option query params
+    '\\-\\.\\+\\!\\*\\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?' +
+    "(?:\\b|$)"
+  );
+  rEmailAddr = new RegExp(
+    '[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}' +
+    '\\@' +
+    '[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}' +
+    '(' +
+    '\\.' +
+    '[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}' +
+    ')+'
+  );
+  rSplit = /\s/;
+
+  escapeMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    "'": '&#39;',
+    '"': '&quot;'
+  };
+  function escape( str ) {
+    return str.replace(/[&<>'"]/g, function( $1 ) {
+      return escapeMap[ $1 ];
+    });
+  }
+
+  function decodeUri( str ) {
+    try {
+      str = decodeURI( str );
+    } catch( err ) {}
+
+    return str;
+  }
+
+  function parse( str, begin, length, fn  ) {
+    return escape( str.substring( 0, begin ) ) + fn() + escape( str.substring( begin + length ) );
+  }
+
+  return function( text ) {
+    var strs,
+        matchs;
+
+    strs = text.split( rSplit );
+    _.forEach( strs, function( str, index, strs ) {
+      if ( str === '' ) {
+        strs[ index ] = '\r\n';
+      } else {
+        if ( matchs = rEmailAddr.exec( str ) ) {      // email
+          // console.log('Email ::: ', matchs );
+          strs[ index ] = parse( str, str.indexOf( matchs[ 0 ] ), matchs[ 0 ].length, function () {
+            return decodeUri( '<a href="mailto:' + matchs[ 0 ] + '">' + matchs[ 0 ] + '</a>' );
+          });
+        } else if ( matchs = rWebUrl.exec( str ) ) {  // Uri, Url
+          // console.log('Uri, Url ::: ', matchs );
+          strs[ index ] = parse( str, str.indexOf( matchs[ 0 ] ), matchs[ 0 ].length, function () {
+            return decodeUri( '<a href="' + ( matchs[ 2 ] ? '' : DEFAULT_PROTO ) + matchs[ 0 ] + '" target="_blank">' + matchs[ 0 ] + '</a>' );
+          });
+        } else {
+            strs[ index ] = escape( str );
+        }
+      }
+    });
+
+    // console.log( strs.join(" ") );
+    return strs.join(" ");
+  };
+});
+
+// tobe no longer used
 app.filter('parseUrl', function() {
   //URLs starting with http://, https://, or ftp://
   //var urls = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
