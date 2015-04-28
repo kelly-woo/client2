dd3a01d506010c314a9c7980beb00a9364bc7fc7
Mark <mark.park@tosslab.com>
2015-04-28 10:42:01 +0900

    fix(file.upload): Fix integration   - integration menu remove

--- client/app/center/center_chat_input_box/center.chat.input.box.directive.js
+++ client/app/center/center_chat_input_box/center.chat.input.box.directive.js
@@ -17,32 +17,39 @@
     };
 
     function link(scope, element, attrs) {
-      var menu = element.find('#integration-menu'),
-          uploadMap = {
-            'client': function(ele) {
-              $('<input type="file" ' + (multiple ? 'multiple' : '') + ' />')
-                .on('change', function(evt) {
-                  scope.onFileSelect(evt.target.files);
-                })
-                .trigger('click');
-            },
-            'google-drive': function(ele) {
-              integrationService.createGoogleDrive(scope, ele, {multiple: multiple});
-            },
-            'dropbox': function(ele) {
-              integrationService.createDropBox(scope, ele, {multiple: multiple});
-            }
-          };
+      // var menu = element.find('#integration-menu'),
+      //     uploadMap = {
+      //       'client': function(ele) {
+      //         $('<input type="file" ' + (multiple ? 'multiple' : '') + ' />')
+      //           .on('change', function(evt) {
+      //             scope.onFileSelect(evt.target.files);
+      //           })
+      //           .trigger('click');
+      //       },
+      //       'google-drive': function(ele) {
+      //         integrationService.createGoogleDrive(scope, ele, {multiple: multiple});
+      //       },
+      //       'dropbox': function(ele) {
+      //         integrationService.createDropBox(scope, ele, {multiple: multiple});
+      //       }
+      //     };
 
-      menu
-        .on('click', 'li', function() {
-          var className = this.className,
-              fn;
+      // menu
+      //   .on('click', 'li', function() {
+      //     var className = this.className,
+      //         fn;
 
-          if (fn = uploadMap[className]) {
-            fn(this);
-          }
-        });
+      //     if (fn = uploadMap[className]) {
+      //       fn(this);
+      //     }
+      //   });
+      element.find('#primary_file_button').on('click', function() {
+        $('<input type="file" ' + (multiple ? 'multiple' : '') + ' />')
+          .on('change', function(evt) {
+            scope.onFileSelect(evt.target.files);
+          })
+          .trigger('click');
+      });
     }
   }
 })();
