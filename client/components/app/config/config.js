945dc5609209a576af8b6e25998afa0d622f99f6
Mark <mark.park@tosslab.com>
2015-05-14 01:22:30 +0900

    live deploy....

--- client/components/app/config/config.js
+++ client/components/app/config/config.js
@@ -8,28 +8,28 @@
   angular
     .module('app.config', [])
     .constant('configuration', {
-      name                : 'local',
-      api_address         : 'http://i1.jandi.io:8888/',
-      socket_address      : 'http://ws.jandi.io:8888/',
+      name                : 'staging',
+      api_address         : '../',
+      socket_address      : 'https://ws.jandi.com/',
       api_version         : '2',
-      ga_token            : 'UA-54051037-1',
-      ga_token_global     : 'UA-54051037-4',
-      mp_account_token    : '322e7bff6504a2f680c0f2d4a3545319',
-      mp_member_token     : '081e1e9730e547f43bdbf59be36a4e31',
-      base_url            : '.jandi.io:8888',
-      assets_url          : 'http://local.jandi.io:9000/',
-      base_protocol       : 'http://',
-      main_address        : 'http://www.jandi.io:8888/main/#/',
+      ga_token            : 'UA-53634725-1',
+      ga_token_global     : 'UA-53634725-4',
+      mp_account_token    : 'fe754ed3c3b6fcf0c510ebcfd6244f3e',
+      mp_member_token     : 'f72e67d5099ab4e0ba4a83da4a9cafe3',
+      base_url            : '.jandi.com',
+      assets_url          : 'https://www.jandi.com/app/',
+      base_protocol       : 'https://',
+      main_address        : 'https://www.jandi.com/main/#/',
       app_store_address   : 'itms-apps://itunes.apple.com/us/app/jandi/id904895208?mt=8',
       play_store_address  : 'https://play.google.com/store/apps/details?id=com.tosslab.jandi.app',
-      last_update_message : '04/06/2014 22:40:00',
+      last_update_message : '04/06/2015 23:00:00',
       integration: {
         google_drive: {
-          client_id: '480258439134-5jutkf7pn2nupra8t0ijj7b1pocbb0ob.apps.googleusercontent.com',
-          api_key: 'AIzaSyDkMnioQdS5UuhX7uw5WNHM3GxgK-nImFY',
-          redirect: 'http://i1.jandi.io:8888/'
+          client_id: '524738787050-7dvpmffvpmk3ooaifcl2a9kpqsm17l12.apps.googleusercontent.com',
+          api_key: 'AIzaSyAarJcX7VfgOiuYzl0ShapunGh5B7amAxA',
+          redirect: 'https://oauth.jandi.com/'
         },
-        dropbox: '4hbb9l5wu46okhp'
+        dropbox: 'sce84hy9tjy3o6v'
       }
     })
     .run(run);
