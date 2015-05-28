3c8df981cdbc1a225fd863fcb863034b3a18056e
Mark <mark.park@tosslab.com>
2015-05-28 17:51:07 +0900

    fix(library): convert indentation to spaces

--- client/assets/javascripts/core.js
+++ client/assets/javascripts/core.js
@@ -1,33 +1,33 @@
 (function(window) {
-    /**
-     * jnd library
-     */
-    var jnd = {};
+  /**
+   * jnd library
+   */
+  var jnd = {};
 
+  /**
+   * object inheritance pattern을 사용할때 최상위 object
+   */
+  jnd.object = {
     /**
-     * object inheritance pattern을 사용할때 최상위 object
+     * OLOO(objects linked to other objects) style로 inheritance을 위한 object 생성
+     * @param {object} prop - object properties
      */
-    jnd.object = {
-        /**
-         * OLOO(objects linked to other objects) style로 inheritance을 위한 object 생성
-         * @param {object} prop - object properties
-         */
-        make: function(prop) {
-            var that = this;
+    make: function(prop) {
+      var that = this;
       var obj;
 
       obj = Object.create(that);
 
       return that.mixin(prop, obj);
-        },
-        /**
-         * object와 object의 property를 base object에 섞음
-         * @param {object} prop - base의 properties object
-         * @param {object} base - base object
-         */
-        mixin: function(prop, base) {
-            var that = this;
-            var target;
+    },
+    /**
+     * object와 object의 property를 base object에 섞음
+     * @param {object} prop - base의 properties object
+     * @param {object} base - base object
+     */
+    mixin: function(prop, base) {
+      var that = this;
+      var target;
       var e;
 
       target = base || {};
@@ -38,11 +38,11 @@
       }
 
       return target;
-        }
-    };
+    }
+  };
 
-    jnd.util = {};
+  jnd.util = {};
 
-    // Global variable
-    window.jnd = jnd;
+  // Global variable
+  window.jnd = jnd;
 })(window);
