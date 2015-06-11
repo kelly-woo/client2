9308b9fe5590df8c4eca703237965ef661c22c9a
jihoon Kim <jihoonk@tosslab.com>
2015-06-11 11:13:24 +0900

    fix(message.search): Apply code review comment.   - Add comment.

--- client/app/right/file.detail/file.detail.directive.js
+++ client/app/right/file.detail/file.detail.directive.js
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview file detail page 에서 현재 파일이 이미지일 경우에만 preview 를 보여준다.
+ * @author JiHoon Kim <jihoonk@tosslab.com>
+ */
 (function() {
   'use strict';
 
@@ -5,25 +9,24 @@
     .module('jandiApp')
     .directive('fileDetailPreview', fileDetailPreview);
 
+  /* @ngInject */
   function fileDetailPreview($rootScope, $timeout) {
     return {
       restrict: 'A',
       link: link
     };
 
-    function link($scope, jqEle) {
+    function link(scope, jqEle) {
       if ($rootScope.setFileDetailCommentFocus) {
         $rootScope.setFileDetailCommentFocus = false;
-
         $timeout(function() {
-          $scope.onCommentFocusClick();
+          scope.onCommentFocusClick();
         });
       }
 
-      if ($scope.ImageUrl) {
-        jqEle = jqEle.show().children('img').attr({ src: $scope.ImageUrl });
-
-        $scope.cursor && jqEle.css({ cursor: $scope.cursor });
+      if (scope.ImageUrl) {
+        jqEle = jqEle.show().children('img').attr({ src: scope.ImageUrl });
+        scope.cursor && jqEle.css({ cursor: scope.cursor });
       } else {
         jqEle.hide();
       }
