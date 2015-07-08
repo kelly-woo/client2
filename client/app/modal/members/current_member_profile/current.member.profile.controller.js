838410b4ff6dc322a5d81492b252eb1d768e94d9
jihoon Kim <jihoonk@tosslab.com>
2015-07-08 18:32:42 +0900

    fix(current.member.profile): Fix typo.

--- client/app/modal/members/current_member_profile/current.member.profile.controller.js
+++ client/app/modal/members/current_member_profile/current.member.profile.controller.js
@@ -55,7 +55,7 @@
      * @private
      */
     function _setProfileImg() {
-      $scope.profileImg =  $filter('getMediumThumbnail2')($scope.curUser);
+      $scope.profileImg =  $filter('getMediumThumbnail')($scope.curUser);
     }
 
     /**
