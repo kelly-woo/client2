ae609a62bdbfe73e69be8ea208bc47c76dfcab69
Mark <mark.park@tosslab.com>
2015-07-08 14:45:42 +0900

    fix(clipboard): Fix plain text in clipboardData

--- client/app/center/view_components/center_chat_input_box/center.chat.input.box.directive.js
+++ client/app/center/view_components/center_chat_input_box/center.chat.input.box.directive.js
@@ -48,35 +48,39 @@
 
       if (Browser.chrome) {
         ImagePaste.createInstance(messageInput, {
-          // image data 되기 직전 event handler
-          onImageLoading: function() {
+          // content data 되기 직전 event handler
+          onContentLoading: function() {
             scope.isLoading = true;
           },
-          // image load 된 후 event handler
-          onImageLoad: function(data) {
-            scope.onFileSelect([data], {
-              createFileObject: function _createFileObject(data) {
-                var blob = fileAPIservice.dataURItoBlob(data);
-                // message-input에 입력된 text를 file comment로 설정함
-                var comment = messageInput.val();
+          // content load 된 후 event handler
+          onContentLoad: function(type, data) {
+            if (type === 'text') {
+              messageInput.val(data).trigger('change');
+            } else if (type === 'image') {
+              scope.onFileSelect([data], {
+                createFileObject: function _createFileObject(data) {
+                  var blob = fileAPIservice.dataURItoBlob(data);
+                  // message-input에 입력된 text를 file comment로 설정함
+                  var comment = messageInput.val();
 
-                messageInput.val('').trigger('change');
+                  messageInput.val('').trigger('change');
 
-                return {
-                  name: 'Image_' + $filter('date')((new Date()).getTime(), 'yyyy-MM-dd HH:mm:ss') + '.png',
-                  type: 'image/png',
-                  blob: blob,
-                  size: blob.size,
-                  uploadType: 'clipboard',
-                  dataUrl: data,
+                  return {
+                    name: 'Image_' + $filter('date')((new Date()).getTime(), 'yyyy-MM-dd HH:mm:ss') + '.png',
+                    type: 'image/png',
+                    blob: blob,
+                    size: blob.size,
+                    uploadType: 'clipboard',
+                    dataUrl: data,
 
-                  comment: comment
-                };
-              }
-            });
+                    comment: comment
+                  };
+                }
+              });
+            }
           },
-          // image load 된 후 image load시 변경된 상태가 정리된 후 event handler
-          onImageLoaded: function() {
+          // content load 된 후 image load시 변경된 상태가 정리된 후 event handler
+          onContentLoaded: function() {
             scope.isLoading = false;
           }
         });
