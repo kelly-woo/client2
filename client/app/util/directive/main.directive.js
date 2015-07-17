34020621ed89d3828bf8071561bc2a8cb2e59f1d
young.park <young.park@tosslab.com>
2015-07-17 21:08:43 +0900

    hotfix(elastic): input 입력시 느려지는 현상 수정    - text elastic 내재화    - sticker 아이콘 레이아웃 구조 수정 및 불필요한 이벤트 제거

--- client/app/util/directive/main.directive.js
+++ client/app/util/directive/main.directive.js
@@ -2,6 +2,142 @@
 
 var app = angular.module('jandiApp');
 
+/**
+ * jandi textarea elastic 디렉티브
+ */
+app.directive('jndElastic', function($timeout, Browser, jndPubSub) {
+    return {
+    restrict: 'A',
+    scope: false,
+    link: link
+  };
+  function link(scope, el, attrs) {
+    var _jqMirror;
+    var _resizeTimer;
+    var _height;
+    var _maxHeight;
+    var _paddingVertical;
+    var _paddingHorizontal;
+
+    _init();
+
+    /**
+     * 초기화
+     * @private
+     */
+    function _init() {
+      _initializeMirror();
+      _attachEvents();
+      _attachDomEvents();
+    }
+
+    /**
+     * 이벤트 리스너를 바인딩한다.
+     * @private
+     */
+    function _attachEvents() {
+      scope.$on('$destroy', _onDestroy);
+    }
+
+    /**
+     * 소멸자
+     * @private
+     */
+    function _onDestroy() {
+      _detachDomEvents();
+    }
+
+    /**
+     * 높이를 계산할 mirror 엘리먼트를 생성한다.
+     * @private
+     */
+    function _initializeMirror() {
+      var mirrorInitStyle = 'position: absolute; top: -999px; right: auto; bottom: auto;' +
+        'left: 0; overflow: scroll; -webkit-box-sizing: content-box;' +
+        '-moz-box-sizing: content-box; box-sizing: content-box;' +
+        'min-height: 0 !important; height: 0 !important; padding: 0;' + +
+        'word-wrap: break-word; border: 0;';
+      var copyStyle = [
+        'fontFamily',
+        'fontSize',
+        'fontWeight',
+        'fontStyle',
+        'letterSpacing',
+        'lineHeight',
+        'textTransform',
+        'wordSpacing',
+        'textIndent'
+      ];
+
+      _jqMirror = $('<textarea tabindex="-1" ' +
+        'style="' + mirrorInitStyle + '"/>');
+
+      _.forEach(copyStyle, function(value) {
+        _jqMirror.css(value, el.css(value));
+      });
+
+      _maxHeight = parseInt(el.css('maxHeight'), 10);
+      _paddingVertical = parseInt(el.css('paddingBottom'), 10) + parseInt(el.css('paddingTop'), 10);
+      _paddingHorizontal = parseInt(el.css('paddingLeft'), 10) + parseInt(el.css('paddingRight'), 10);
+
+      //@fixme 레이아웃이 ie 와 chrome 이 맞지 않는 현상이 발생하여 임시로 예외처리 해놓음
+      if (!Browser.msie) {
+        _paddingVertical = 0;
+      }
+
+      $('body').append(_jqMirror);
+      _height = _jqMirror.height();
+    }
+
+    /**
+     * dom 이벤트를 attach 한다.
+     * @private
+     */
+    function _attachDomEvents() {
+      el.on('propertychange change click keyup input paste', _onChange);
+    }
+
+    /**
+     * dom 이벤트를 detach 한다.
+     * @private
+     */
+    function _detachDomEvents() {
+      el.off('propertychange change click keyup input paste', _onChange);
+      _jqMirror.remove();
+    }
+
+    /**
+     * resize 를 수행한다.
+     * @private
+     */
+    function _resize() {
+      var height;
+      _jqMirror.width(el.width() + _paddingHorizontal);
+      _jqMirror.val(el.val());
+
+      height = _jqMirror[0].scrollHeight;
+      if (_maxHeight !== 0 && _maxHeight < height) {
+        height = _maxHeight;
+      }
+      if (_height !== height) {
+        el.height(height + _paddingVertical);
+        jndPubSub.pub('elastic:resize');
+      }
+    }
+
+    /**
+     * onChange 이벤트 핸들러
+     * @param {event} keyEvent
+     * @private
+     */
+    function _onChange(keyEvent) {
+      $timeout.cancel(_resizeTimer);
+      _resizeTimer = $timeout(_resize, 10);
+    }
+  }
+});
+
+
 app.directive('messageSubmit', function() {
   return {
     restrict: 'A',
