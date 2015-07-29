69efe460f866e5b22400a1ae5237b6bfa6423ecc
young.park <young.park@tosslab.com>
2015-07-29 20:27:48 +0900

    fix(mention): hasEventHandler 옵션 추가

--- client/app/util/filter/mention.html.decode.filter.js
+++ client/app/util/filter/mention.html.decode.filter.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview 사용자가 입력한 text를 수정하여 center chat에 출력함.
+ * @fileoverview HtmlEntity 로 변환된 mention 태그만 html 문자열로 디코드 한다.
  */
 (function() {
   'use strict';
@@ -16,7 +16,7 @@
     function decode(text) {
       var decodeFilter = $filter('htmlDecode');
       text = text || '';
-      return text.replace(/\&lt\;a mention\-view\=.+\/a\&gt;/g, function(m0) {
+      return text.replace(/\&lt\;(\w+)\smention\-view\=.+\/\1\&gt;/g, function(m0) {
         return decodeFilter(m0);
       });
     }
