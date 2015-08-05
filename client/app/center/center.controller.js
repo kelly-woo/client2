db549f06bf6a8b9dc8e62ef4ae912d2512c490a5
young.park <young.park@tosslab.com>
2015-08-05 23:45:44 +0900

    fix(center): elastic scrollToBottom 활성화

--- client/app/center/center.controller.js
+++ client/app/center/center.controller.js
@@ -1585,7 +1585,7 @@ app.controller('centerpanelController', function($scope, $rootScope, $state, $fi
     var jqInput = $('#message-input');
     // center controller의 content load가 완료 된 상태이고 chat 스크롤이 최 하단에 닿아있을때 scroll도 같이 수정
     if ($scope.isInitialLoadingCompleted && _isBottomReached()) {
-      //_scrollToBottom();
+      _scrollToBottom();
     }
     _jqContainer.css('margin-bottom', jqInput.height() - 29);
   }
