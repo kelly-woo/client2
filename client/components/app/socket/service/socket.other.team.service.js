4d9794af8e68e3a56be2d9f22cfd9e80e2f3ad3b
jihoon Kim <jihoonk@tosslab.com>
2015-08-17 11:42:52 +0900

    feat(team.socket): Update room notification setting on other team's socket event.

--- client/components/app/socket/service/socket.other.team.service.js
+++ client/components/app/socket/service/socket.other.team.service.js
@@ -11,14 +11,10 @@
   /* @ngInject */
   function jndWebSocketOtherTeamManager($timeout, accountService, OtherTeamNotification,
                                         EntityMapManager, memberService) {
-    var _timeoutCaller;
-    var _isWaiting = false;
-    var _waitingMap = {};
-
     var _teamStatusMap = {};
 
-
     var OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP = 'other_team_topic_status';
+    var ROOM_SUBSCRIPTION_UPDATED = 'room_subscription_updated';
 
     // 연속된 api call를 방지하기위해 기다리는 시간
     //var paddingTime = 5000;
@@ -31,7 +27,9 @@
      * @param socketEvent
      */
     function onSocketEvent(socketEvent) {
-      if (_isNewMessage(socketEvent) && !!socketEvent.teamId) {
+      if (_isRoomSubscriptionUpdated(socketEvent)) {
+        _onRoomSubscriptionUpdated(socketEvent);
+      } else if (_isNewMessage(socketEvent) && !!socketEvent.teamId) {
         // 처리하려는 소켓이벤트는 무조건 팀아이디와 방의 정보가 있어야한다.
 
         // file_comment 일 경우 방의 정보가 rooms로 넘어오기때문에 처리한다.
@@ -42,6 +40,55 @@
           _sendBrowserNotification(socketEvent);
         }
       }
+    }
+
+    /**
+     * 다른 팀의 토픽들중 하나의 subscription status가 변경되었을 때 호출된다.
+     * @param {object} socketEvent - socket event로 들어온 parameter
+     * @private
+     */
+    function _onRoomSubscriptionUpdated(socketEvent) {
+      var messageMarkersMap;
+
+      var data = socketEvent.data;
+
+      var teamId = data.teamId;
+      var roomId = data.roomId;
+      var newValue = data.subscribe;
+
+      if (_hasTeamMessageMarkers(teamId)) {
+        // 팀에 대한 정보가 있을때
+        messageMarkersMap = EntityMapManager.get(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId);
+        var roomInfo = messageMarkersMap[roomId];
+
+        if (_.isUndefined(roomInfo)) {
+          // 방에 대한 정보가 없을때 새로 만듬
+          roomInfo = {
+            subscribe: newValue
+          }
+        } else {
+          // 방에 대한 정보가 있을 때에는, 기존 정보를 업데이트
+          roomInfo['subscribe'] = newValue;
+        }
+
+        messageMarkersMap[roomId] = roomInfo;
+      } else {
+        // 팀에 대한 정보가 없을 때
+        var tempMessageMarkerObj = {
+          roomId: {
+            subscribe: newValue
+          }
+        };
+
+        // 새로 만든다
+        EntityMapManager.set(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId, tempMessageMarkerObj);
+        // todo: 아니면 그냥 그 팀 정보를 다 가져올까요?
+        //_getTeamMessageMarkers(teamId, socketEvent);
+
+      }
+
+
+
     }
 
     /**
@@ -82,7 +129,17 @@
         return false;
       }
 
-      return _isSubscriptionOn(teamId, socketEvent.room.id);
+      return _isSubscriptionOn(teamId, socketEvent);
+    }
+
+    /**
+     * local에 팀의 message markers를 들고 있는지 없는지 확인한다.
+     * @param {number} teamId - 알고 싶은 팀의 아이디
+     * @returns {*}
+     * @private
+     */
+    function _hasTeamMessageMarkers(teamId) {
+      return EntityMapManager.contains(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId);
     }
 
     /**
@@ -113,13 +170,145 @@
     }
 
     /**
-     * local에 팀의 message markers를 들고 있는지 없는지 확인한다.
+     * entityId를 key로 하는 맵을 새로 만든다.
+     * @param {array} messageMarkers - server로부터 내려온 member model에 있는 u_messageMarkers와 동일
+     * @returns {{}}
+     * @private
+     */
+    function _getMessageMarkersMap(messageMarkers) {
+      var _messageMarkers = {};
+
+      _.forEach(messageMarkers, function(messageMarker) {
+        _messageMarkers[messageMarker.entityId] = messageMarker;
+      });
+
+      return _messageMarkers;
+
+    }
+
+    /**
+     * teamId에 해당하는 팀에 있는 방들 중 roomId를 가진 방의 subscibe 값을 리턴한다.
      * @param {number} teamId - 알고 싶은 팀의 아이디
+     * @param {object} socketEvent - socket event parameter
+     * @returns {.data.subscribe|*}
+     * @private
+     */
+    function _isSubscriptionOn(teamId, socketEvent) {
+      var messageMarkersMap = EntityMapManager.get(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId);
+      var roomId = socketEvent.room.id;
+      if (_.isUndefined(messageMarkersMap[roomId])) {
+        // 방에대한 정보가 없을 때.
+        _getTeamMessageMarkers(teamId, socketEvent);
+        return false;
+      }
+
+      return messageMarkersMap[roomId].subscribe;
+
+    }
+
+    /**
+     * socketEvent으로 browser notification를 날린다.
+     * @param {object} socketEvent - socket event
+     * @private
+     */
+    function _sendBrowserNotification(socketEvent) {
+      var teamId = socketEvent.teamId;
+      var timeoutCaller;
+
+      if (_hasTimeoutCaller(teamId)) {
+        timeoutCaller = _getTimeoutCaller(teamId);
+        $timeout.cancel(timeoutCaller);
+      }
+
+      timeoutCaller = $timeout(function() {
+        // 어카운트 정보를 업데이트해야 화면 왼쪽 상단에 위치한 '팀' 메뉴에 붙어있는 파란색 점을 업데이트할 수 있음
+        accountService.updateCurrentAccount();
+
+        OtherTeamNotification.addNotification(socketEvent);
+
+        _afterNotificationSent(teamId, socketEvent);
+      }, paddingTime);
+
+      _setTimeoutCaller(teamId, timeoutCaller);
+
+    }
+
+    /**
+     * 해당 팀에 대한 browser notification을 보낸 후 처리해야할 일들의 모음이다.
+     * @param {number} teamId - browser notification을 보낸 팀의 아이디
+     * @param {object} socketEvent - socket event로 들어온 paramter
+     * @private
+     */
+    function _afterNotificationSent(teamId, socketEvent) {
+      // 혹시 모르니 $timoue을 cancel 시킨다.
+      var timeout = _getTimeoutCaller(teamId);
+      $timeout.cancel(timeout);
+    }
+
+    /**
+     * 해당 팀아이디의 정보를 호출하고 있는지 확인한다.
+     * @param {number} teamId - 확인해보고 싶은 팀의 아이디
+     * @returns {boolean}
+     * @private
+     */
+    function _isWaitingOnTeamId(teamId) {
+      return !!_teamStatusMap[teamId] && _teamStatusMap[teamId]['isWaiting'];
+    }
+
+    /**
+     * 해당 팀의 정보를 얻고있다는 flag를 설정한다.
+     * @param {number} teamId - 설정하려는 플래그의 팀의 아이디
+     * @param {boolean} value - 설정하려는 값
+     * @private
+     */
+    function _setWaiting(teamId, value) {
+      var curValue = _teamStatusMap[teamId];
+
+      if (_.isUndefined(curValue)) {
+        _teamStatusMap[teamId] = {
+          isWaiting: value
+        };
+      } else {
+        curValue.isWaiting = value;
+      }
+    }
+
+    /**
+     * 해당 teamId로 이미 timeout이 실행되고있는지 없는지 확인한다.
+     * @param {number} teamId - 확인하고 싶은 팀의 아이디
+     * @returns {boolean|*}
+     * @private
+     */
+    function _hasTimeoutCaller(teamId) {
+      return !!_teamStatusMap[teamId] && !!_teamStatusMap[teamId]['timeoutCaller'] && _teamStatusMap[teamId]['timeoutCaller'];
+    }
+
+    /**
+     * 해당 teamId로 실행된 timeout을 저장한다.
+     * @param {number} teamId - timeout을 실행한 팀의 아이디
+     * @param {object} caller - timeout promise
+     * @private
+     */
+    function _setTimeoutCaller(teamId, caller) {
+      var curValue = _teamStatusMap[teamId];
+
+      if (_.isUndefined(curValue)) {
+        _teamStatusMap[teamId] = {
+          timeoutCaller: caller
+        }
+      } else {
+        curValue.timeoutCaller = caller;
+      }
+    }
+
+    /**
+     * 해당 teamId로 실행되어지고 있는 timeout을 가져온다.
+     * @param {number} teamId - timeout을 실행시키고 있는 팀의 아이디
      * @returns {*}
      * @private
      */
-    function _hasTeamMessageMarkers(teamId) {
-      return EntityMapManager.contains(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId);
+    function _getTimeoutCaller(teamId) {
+      return _teamStatusMap[teamId]['timeoutCaller'];
     }
 
     /**
@@ -143,113 +332,15 @@
     }
 
     /**
-     * entityId를 key로 하는 맵을 새로 만든다.
-     * @param {array} messageMarkers - server로부터 내려온 member model에 있는 u_messageMarkers와 동일
-     * @returns {{}}
-     * @private
-     */
-    function _getMessageMarkersMap(messageMarkers) {
-      var _messageMarkers = {};
-
-      _.forEach(messageMarkers, function(messageMarker) {
-        _messageMarkers[messageMarker.entityId] = messageMarker;
-      });
-
-      return _messageMarkers;
-
-    }
-
-    /**
-     * teamId에 해당하는 팀에 있는 방들 중 roomId를 가진 방의 subscibe 값을 리턴한다.
-     * @param {number} teamId - 알고 싶은 팀의 아이디
-     * @param {number} roomId - 알고 싶은 방의 아이디
-     * @returns {.data.subscribe|*}
-     * @private
-     */
-    function _isSubscriptionOn(teamId, roomId) {
-      var messageMarkersMap = EntityMapManager.get(OTHER_TEAM_TOPIC_NOTIFICATION_STATUS_MAP, teamId);
-      if (_.isUndefined(messageMarkersMap[roomId])) {
-        return _getTeamMessageMarkers(teamId, roomId);
-      }
-
-      return messageMarkersMap[roomId].subscribe;
-
-    }
-
-    /**
-     * socketEvent으로 browser notification를 날린다.
-     * @param {object} socketEvent - socket event
-     * @private
-     */
-    function _sendBrowserNotification(socketEvent) {
-      var teamId = socketEvent.teamId;
-      var timeoutCaller;
-
-      if (_hasTimeoutCaller(teamId)) {
-        timeoutCaller = _getTimeoutCaller(teamId);
-        $timeout.cancel(timeoutCaller);
-      }
-
-      timeoutCaller = $timeout(function() {
-        accountService.updateCurrentAccount();
-        //DesktopNotification.addOtherTeamNotification(socketEvent);
-
-        OtherTeamNotification.addNotification(socketEvent);
-      }, paddingTime);
-
-      _setTimeoutCaller(teamId, timeoutCaller);
-
-    }
-
-    /**
-     * 해당 팀아이디의 정보를 호출하고 있는지 확인한다.
-     * @param {number} teamId - 확인해보고 싶은 팀의 아이디
+     * socketEvent가 'room_subscription_updated'인지 아닌지 확인한다.
+     * @param {object} socketEvent - socket event로 들어온 parameter와 동일
      * @returns {boolean}
      * @private
      */
-    function _isWaitingOnTeamId(teamId) {
-      return !!_teamStatusMap[teamId] && _teamStatusMap[teamId]['isWaiting'];
+    function _isRoomSubscriptionUpdated(socketEvent) {
+      return socketEvent.event === ROOM_SUBSCRIPTION_UPDATED;
     }
 
-    /**
-     * 해당 팀의 정보를 얻고있다는 flag를 설정한다.
-     * @param {number} teamId - 설정하려는 플래그의 팀의 아이디
-     * @param {boolean} value - 설정하려는 값
-     * @private
-     */
-    function _setWaiting(teamId, value) {
-      _waitingMap[teamId] = value;
-
-      var curValue = _teamStatusMap[teamId];
-
-      if (_.isUndefined(curValue)) {
-        _teamStatusMap[teamId] = {
-          isWaiting: value
-        };
-      } else {
-        curValue.isWaiting = value;
-      }
-    }
-
-    function _hasTimeoutCaller(teamId) {
-      return !!_teamStatusMap[teamId] && _teamStatusMap[teamId]['timeoutCaller'];
-    }
-
-    function _setTimeoutCaller(teamId, caller) {
-      var curValue = _teamStatusMap[teamId];
-
-      if (_.isUndefined(curValue)) {
-        _teamStatusMap[teamId] = {
-          timeoutCaller: caller
-        }
-      } else {
-        curValue.timeoutCaller = caller;
-      }
-    }
-    function _getTimeoutCaller(teamId) {
-      return _teamStatusMap[teamId]['timeoutCaller'];
-
-    }
     /**
      * socketEvent가 새로운 메세지 혹은 파일 커맨트에 관련된 이벤트인지 아닌지 확인한다.
      * @param {object} socketEvent - socket event object
