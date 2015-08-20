bb8da4e06a0567ca3d12a575690467a073f5c5dd
Mark <tmsla123@gmail.com>
2015-08-20 11:36:46 +0900

    fix(modal): Fix member modal

--- client/app/modal/members/member_list/member.list.directive.js
+++ client/app/modal/members/member_list/member.list.directive.js
@@ -19,21 +19,55 @@
     };
 
     function link(scope, el, attrs) {
-      var target = $(attrs.selector);
       var list = attrs.list;
       var repeat = attrs.repeat;
+      var target;
       var originOnSelect;
       var jqMemberItem;
 
-      if (target.length) {
-        scope.list = list;
-        originOnSelect = scope.$eval(attrs.onSelect);
+      _init();
 
-        $timeout(function() {
-          target.focus();
+      function _init() {
+        target = $(attrs.selector);
+
+        if (target.length) {
+          scope.list = list;
+          originOnSelect = scope.$eval(attrs.onSelect);
+
+          $timeout(function () {
+            target.focus();
+          });
+
+          _on();
+
+          scope.repeatDone = repeatDone;
+          scope.onSelect = onSelect;
+          scope.focusItem = focusItem;
+
+          _createMemberList();
+        }
+      }
+
+      /**
+       * create repeat member item
+       * @private
+       */
+      function _createMemberList() {
+        jqMemberItem = angular.element('<member-item></member-item>');
+        jqMemberItem.attr({
+          'ng-repeat': repeat
         });
+        jqMemberItem = $compile(jqMemberItem)(scope);
 
-        target.on('keydown', function(event) {
+        el.prepend(jqMemberItem);
+      }
+
+      /**
+       * add event listener
+       * @private
+       */
+      function _on() {
+        target.on('keydown', function (event) {
           var which = event.which;
           var members = scope[list];
 
@@ -54,65 +88,56 @@
             }
           }
         });
+      }
 
-        /**
-         * repeat done event handler
-         */
-        scope.repeatDone = function() {
-          if (!target.val()) {
-            // target의 value가 존재하지 않는다면 첫번째 item을 focus
-            scope.focusItem(0);
+      /**
+       * repeat done event handler
+       */
+      function repeatDone() {
+        if (!target.val()) {
+          // target의 value가 존재하지 않는다면 첫번째 item을 focus
+          scope.focusItem(0);
+        }
+      }
+
+      /**
+       * item select event handler
+       * @param {object} member
+       */
+      function onSelect(member) {
+        originOnSelect(member);
+        target.focus();
+      }
+
+      /**
+       * focus item
+       * list에서 item이 focus된 상태를 표현함. focus된 item이 list에서
+       * 보여지지 않는다면 보이도록 list의 scoll을 조정함.
+       * @param {number} matchIndex
+       */
+      function focusItem(matchIndex) {
+        var jqMentionItem;
+        var itemPosition;
+        var contPosition;
+        var scrollTop;
+        var compare;
+
+        jqMentionItem = el.children().eq(matchIndex);
+        if (jqMentionItem.length) {
+          scrollTop = el.scrollTop();
+
+          itemPosition = $position.offset(jqMentionItem);
+          contPosition = $position.offset(el);
+
+          compare = itemPosition.top - contPosition.top;
+          if (compare < 0) {
+            el.scrollTop(scrollTop + compare);
+          } else if (compare + itemPosition.height > contPosition.height) {
+            el.scrollTop(scrollTop + compare - contPosition.height + itemPosition.height);
           }
-        };
 
-        /**
-         * item select event handler
-         * @param {object} member
-         */
-        scope.onSelect = function(member) {
-          originOnSelect(member);
-          target.focus();
-        };
-
-        /**
-         * focus item
-         * list에서 item이 focus된 상태를 표현함. focus된 item이 list에서
-         * 보여지지 않는다면 보이도록 list의 scoll을 조정함.
-         * @param {number} matchIndex
-         */
-        scope.focusItem = function(matchIndex) {
-          var jqMentionItem;
-          var itemPosition;
-          var contPosition;
-          var scrollTop;
-          var compare;
-
-          jqMentionItem = el.children().eq(matchIndex);
-          if (jqMentionItem.length) {
-            scrollTop = el.scrollTop();
-
-            itemPosition = $position.offset(jqMentionItem);
-            contPosition = $position.offset(el);
-
-            compare = itemPosition.top - contPosition.top;
-            if (compare < 0) {
-              el.scrollTop(scrollTop + compare);
-            } else if ( compare + itemPosition.height > contPosition.height ) {
-              el.scrollTop(scrollTop + compare - contPosition.height + itemPosition.height);
-            }
-
-            scope.setActiveIndex(matchIndex);
-          }
-        };
-
-        // create repeat member item
-        jqMemberItem = angular.element('<member-item></member-item>');
-        jqMemberItem.attr({
-          'ng-repeat': repeat
-        });
-        jqMemberItem = $compile(jqMemberItem)(scope);
-
-        el.prepend(jqMemberItem);
+          scope.setActiveIndex(matchIndex);
+        }
       }
     }
   }
