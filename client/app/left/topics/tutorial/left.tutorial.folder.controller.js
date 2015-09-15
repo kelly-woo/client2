ff91fdc7ce4d822580e40d03f4e17953e01faf4e
young.park <young.park@tosslab.com>
2015-09-15 17:02:09 +0900

    fix(left): 폴더 튜토리얼 다시보지 않기 추가

--- client/app/left/topics/tutorial/left.tutorial.folder.controller.js
+++ client/app/left/topics/tutorial/left.tutorial.folder.controller.js
@@ -1,3 +1,6 @@
+/**
+ * @fileoverview 폴터 튜토리얼
+ */
 (function() {
   'use strict';
 
@@ -8,7 +11,7 @@
   function LeftTutorialFolderCtrl($scope) {
     $scope.isShown = false;
     $scope.close = close;
-
+    $scope.closeAndNeverShow = closeAndNeverShow;
     _init();
 
     /**
@@ -20,10 +23,17 @@
     }
 
     /**
-     *
+     * 닫기
      */
     function close() {
       $scope.isShown = false;
+    }
+
+    /**
+     * 다시 보지 않기
+     */
+    function closeAndNeverShow() {
+      close();
     }
   }
 })();
