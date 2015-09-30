75fd1c0b75a2a0112a1894cc31eac3c15a812b6a
Mark <tmsla123@gmail.com>
2015-09-30 18:40:15 +0900

    fix(center): thumbnail image 출력시 background color는 opacity에 영향 받지 않도록 수정

--- client/assets/javascripts/handlebars.templates.js
+++ client/assets/javascripts/handlebars.templates.js
@@ -153,9 +153,9 @@ this["Handlebars"]["templates"]["center.file"] = Handlebars.template({"1":functi
 },"5":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "              <div class=\"medium-thumbnail image_wrapper msg-file-body__img__background cursor_pointer opac-zero _fileExpand\" image-loader=\""
+  return "              <div class=\"medium-thumbnail image_wrapper msg-file-body__img__background cursor_pointer _fileExpand\">\n                <div class=\"opac-zero\" image-loader=\""
     + this.escapeExpression(this.lambda(((stack1 = (depth0 != null ? depth0.file : depth0)) != null ? stack1.imageUrl : stack1), depth0))
-    + "\" image-max-width=\"360\" image-max-height=\"270\" image-is-center=\"true\"></div>\n";
+    + "\" image-max-width=\"360\" image-max-height=\"270\" image-is-center=\"true\"></div>\n              </div>\n";
 },"7":function(depth0,helpers,partials,data) {
     var stack1;
 
