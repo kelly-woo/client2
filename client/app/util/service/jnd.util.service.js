83e9bdf878d204b5ba51a5e01a0087a6a8e63c06
young.park <young.park@tosslab.com>
2015-12-07 15:01:02 +0900

    chore(util): parseUrl 메서드 추가
      - JndUtil 에 get 파라미터 파싱하는 메서드 추가

--- client/app/util/service/jnd.util.service.js
+++ client/app/util/service/jnd.util.service.js
@@ -11,6 +11,7 @@
   function JndUtil(Dialog, $filter) {
     this.safeApply = safeApply;
     this.alertUnknownError = alertUnknownError;
+    this.parseUrl = parseUrl;
 
     /**
      * angular 의 $apply 를 안전하게 수행한다.
@@ -53,6 +54,29 @@
         allowHtml: true,
         body: body.join('<br />')
       });
+    }
+
+    /**
+     * url 을 파싱하여 get 파라미터로 전달된 데이터를 반환한다.
+     * @returns {{}}
+     */
+    function parseUrl() {
+      var data = {};
+      var url = window.location.href;
+      var urlSplit = url.split('?');
+      var paramsString = urlSplit[1];
+      var tokens;
+      var temp;
+      if (paramsString) {
+        tokens = paramsString.split('&');
+        _.forEach(tokens, function(token) {
+          temp = token.split('=');
+          if (temp.length === 2) {
+            data[temp[0]] = temp[1];
+          }
+        });
+      }
+      return data;
     }
   }
 })();
