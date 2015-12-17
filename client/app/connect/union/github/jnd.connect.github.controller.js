144c7c03abff0ddc40649cb238626219fff3400e
young.park <young.park@tosslab.com>
2015-12-17 18:24:54 +0900

    chore(잔디connect): github 페이지 에서 request payload 생성하도록 기능 추가
      - pubSub event 명 규약에 따르도록 수정
      - footer 를 활용한 request payload 추가

--- client/app/connect/union/github/jnd.connect.github.controller.js
+++ client/app/connect/union/github/jnd.connect.github.controller.js
@@ -1,3 +1,6 @@
+/**
+ * @fileoverview
+ */
 (function() {
   'use strict';
 
@@ -7,11 +10,13 @@
 
   /* @ngInject */
   function JndConnectGithubCtrl($scope, $timeout, JndConnectDummy) {
+    var _originalRepos;
+
     $scope.requestData = {
       mode: 'authed',
       roomId: null,
       botName: 'Github',
-      botThumbnailFile: null,
+      botThumbnailFile: 'https://files.jandi.io/files-profile/94918bd3ab4222a1e5d57d427706164b?size=80',
       lang: 'ko',
       authenticationId: null,
       repo: '',
@@ -28,10 +33,20 @@
       'issue_comment': false,
       'create': false
     };
-    $scope.selectedRoom = null;
-    $scope.selectedRepo = 0;
+
     $scope.isInitialized = false;
     $scope.repositories = [];
+
+    $scope.formData = {
+      roomId: null,
+      repoId: null,
+      branches: '',
+      header: {},
+      footer: {
+        botThumbnailFile: $scope.requestData.botThumbnailFile
+      }
+    };
+
 
     _init();
 
@@ -47,18 +62,33 @@
       }, 1000);
     }
 
+    /**
+     * github 데이터를 가져오기 위한 첫번째 request
+     * @private
+     */
     function _initialRequest() {
       $timeout(_onInitialRequestSuccess, 1000);
     }
 
+    /**
+     * 초기 request 성공 시 이벤트 핸들러
+     * @private
+     */
     function _onInitialRequestSuccess() {
       var githubData = JndConnectDummy.get('github');
+      _originalRepos = githubData.repo.repos;
 
       $scope.repositories =_getSelectboxRepos(githubData.repo.repos);
       $scope.requestData.authenticationId = githubData.repo.authenticationId;
       $scope.isInitialized = true;
     }
 
+    /**
+     * selectbox 에 전달하기 위한 repository 데이터
+     * @param {Array} repos
+     * @returns {Array}
+     * @private
+     */
     function _getSelectboxRepos(repos) {
       var groupList = [];
       var list;
@@ -78,18 +108,32 @@
       return groupList;
     }
 
+    /**
+     * 이벤트 핸들러를 바인딩한다.
+     * @private
+     */
     function _attachEvents() {
       $scope.$on('unionFooter:save', _onSave);
     }
 
+    /**
+     * 설정 저장하기 버튼 클릭 시 이벤트 핸들러
+     * @private
+     */
     function _onSave() {
-      console.log('###github save');
       _setRequestData();
       console.log('$scope.requestData', $scope.requestData);
+      console.log('$scope.formData', $scope.formData);
     }
 
+    /**
+     * request 데이터를 가공한다.
+     * @private
+     */
     function _setRequestData() {
       var events = [];
+      var formData = $scope.formData;
+      var footer = formData.footer;
       _.each($scope.githubEvents, function(value, key) {
         if (value) {
           events.push(key);
@@ -98,7 +142,49 @@
           }
         }
       });
+      _.extend($scope.requestData, {
+        roomId: formData.roomId,
+        repo: _getRepoData(formData.repoId).full_name,
+        branch: _getBranches(),
+        botName: footer.botName,
+        botThumbnailFile: footer.botThumbnailFile
+    });
       $scope.requestData.events = events;
+    }
+
+    /**
+     * 서버 포멧에 맞춘 branch 배열을 반환한다.
+     * @returns {Array}
+     * @private
+     */
+    function _getBranches() {
+      var branches = $scope.formData.branches.split(',');
+      branches = _.map(branches, function(branch) {
+        return _.trim(branch);
+      });
+      return branches;
+    }
+
+    /**
+     * repository ID 를 이용하여 실제 저장소 데이터를 반환한다.
+     * @param {number} repoId
+     * @returns {*}
+     * @private
+     */
+    function _getRepoData(repoId) {
+      var repoData;
+      _.forEach(_originalRepos, function(group) {
+        if (repoData) {
+          return false;
+        }
+        _.forEach(group.lists, function(repo) {
+          if (repo.id === repoId) {
+            repoData = repo;
+            return false;
+          }
+        });
+      });
+      return repoData;
     }
   }
 })();
