64de185b89f496889859ba037b2cce98cf715d4f
young.park <young.park@tosslab.com>
2015-12-22 20:03:53 +0900

    chore(잔디connect): trello 개발 기본 설정 세팅 완료

--- client/app/connect/union/trello/jnd.connect.trello.controller.js
+++ client/app/connect/union/trello/jnd.connect.trello.controller.js
@@ -6,7 +6,9 @@
     .controller('JndConnectTrelloCtrl', JndConnectTrelloCtrl);
 
   /* @ngInject */
-  function JndConnectTrelloCtrl($scope, JndConnect, EntityMapManager) {
+  function JndConnectTrelloCtrl($scope, $q, JndUtil, JndConnect, JndConnectTrelloApi) {
+
+    $scope.isInitialized = false;
 
     _init();
 
@@ -15,6 +17,57 @@
      * @private
      */
     function _init() {
+      $scope.isUpdate = !!$scope.current.connectId;
+      _attachEvents();
+      _initialRequest();
+    }
+
+
+    function _attachEvents() {
+
+    }
+
+    /**
+     * trello 데이터를 가져오기 위한 첫번째 request
+     * @private
+     */
+    function _initialRequest() {
+      var deferred = $q.defer();
+      var promises = [];
+
+      promises.push(JndConnectTrelloApi.getBoards());
+
+      //update 모드가 아닐 경우 바로 view 를 노출한다.
+      if (!$scope.isUpdate) {
+        $scope.isInitialized = true;
+      } else {
+        promises.push(JndConnectTrelloApi.get($scope.current.connectId));
+      }
+
+      $q.all(promises)
+        .then(_onSuccessInitialRequest, _onErrorInitialRequest);
+    }
+
+    /**
+     * initial  request 성공 시 이벤트 핸들러
+     * @param {array} results
+     * @private
+     */
+    function _onSuccessInitialRequest(results) {
+      console.log(results);
+      //_onSuccessGetRepo(results[0].data);
+      //if ($scope.isUpdate) {
+      //  _onSuccessGetSetting(results[1].data);
+      //}
+    }
+
+    /**
+     * initial request 실패 시 이벤트 핸들러
+     * @param results
+     * @private
+     */
+    function _onErrorInitialRequest(results) {
+      JndUtil.alertUnknownError(results);
     }
   }
 })();
