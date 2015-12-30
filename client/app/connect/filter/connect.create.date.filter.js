59aa1d9eb3dfa62bb27ec95f7b2271f989a510b0
young.park <young.park@tosslab.com>
2015-12-30 17:37:01 +0900

    chore(잔디connect): 생성일 노출하는 필터 추가 및 적용

---
+++ client/app/connect/filter/connect.create.date.filter.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview connect 에서
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('jandiApp')
+    .filter('connectCreateDate', unionCreateDate);
+
+  /* @ngInject */
+  function connectCreateDate($filter) {
+    return function(createdAt) {
+      var template = $filter('translate')('@jnd-connect-34');
+      var date = $filter('date')(createdAt, 'yyyy-MM-dd');
+      return template.replace('{{yyyy-mm-dd}}', date);
+    }
+  }
+})();
