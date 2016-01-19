c66ce6c0a747969a5bb9c6cefb9bdcdc9ef2d16f
young.park <young.park@tosslab.com>
2016-01-19 20:20:30 +0900

    chore(preload): preload 빌드 시 모두 생성되어 기존에 추가했던 로직들 제거

--- client/components/jandi/preloader/preloader.js
+++ client/components/jandi/preloader/preloader.js
@@ -23,24 +23,7 @@
     _templateList = [
       'app/disconnect/disconnect.html'
     ];
-    _imgList = [
-      '../assets/images/icon_loading.gif',
-      '../assets/images/icon_network_error.png',
-      '../assets/images/icon-disabled-members-alert.png',
-      // integration icons
-      '../assets/images/icon_google_drive.png',
-      '../assets/images/icon_dropbox.png',
-      // Sample notification icon
-      '../assets/images/jandi-logo-200x200.png',
-      // Favicons
-      '../assets/images/favicon/favicon.png',
-      '../assets/images/favicon/favicon_noti.png',
-      // sticker
-      '../assets/images/sticker/pangya_pangya_off.svg',
-      '../assets/images/sticker/pangya_pangya_on.svg',
-      '../assets/images/sticker/kiyomi_off.svg',
-      '../assets/images/sticker/kiyomi_on.svg'
-    ];
+    _imgList = [];
     _imgList = _imgList.concat(PRELOAD_LIST);
   }
 
