9d8bcf762b7680e56e8eab172f3f01506f1577d5
young.park <young.park@tosslab.com>
2016-01-20 12:02:49 +0900

    fix(잔디connect): 튜토리얼 popover 노출 조건에 video 가 로드 완료 되었는지 여부 판단하도록 추가
      - loadeddata dom 이벤트 핸들러 추가

--- client/app/tutorial/popover/connect/tutorial.popover.connect.directive.js
+++ client/app/tutorial/popover/connect/tutorial.popover.connect.directive.js
@@ -9,7 +9,7 @@
     .module('jandiApp')
     .directive('tutorialPopoverConnect', tutorialPopoverConnect);
 
-  function tutorialPopoverConnect() {
+  function tutorialPopoverConnect(JndUtil, jndPubSub) {
     return {
       link: link,
       scope: {},
@@ -19,6 +19,27 @@
     };
 
     function link(scope, el, attrs) {
+      scope.isVideoLoaded = false;
+
+      _init();
+
+      /**
+       * 초기화 함수
+       * @private
+       */
+      function _init() {
+        el.find('video').on('loadeddata', _onVideoLoad);
+      }
+
+      /**
+       * video load 핸들러
+       * @private
+       */
+      function _onVideoLoad() {
+        JndUtil.safeApply(scope, function() {
+          scope.isVideoLoaded = true;
+        });
+      }
     }
   }
 })();
