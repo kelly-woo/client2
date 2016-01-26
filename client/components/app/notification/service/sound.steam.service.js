aa15fcf2b718fbd5de5bba1a31cbf50943f5c03c
tmsla123 <tmsla123@gmail.com>
2016-01-26 17:11:26 +0900

    chore(notification): bak

---
+++ client/components/app/notification/service/sound.steam.service.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview sound stream service
+ */
+(function() {
+  'use strict';
+  
+  angular
+    .module('jandiApp')
+    .service('SoundStream', SoundStream);
+  
+  function SoundStream() {
+    var AudioContext = window.AudioContext;
+    function load() {
+
+    }
+  }
+})();
