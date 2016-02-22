37054c4e26c042207fcb9cd0e531db86c82ac186
young.park <young.park@tosslab.com>
2016-02-22 16:18:08 +0900

    style(팀목록): 디자인 의견 반영
      - 팀 switch 목록이 window 높이를 넘어간다면 스크롤 바 노출하도록 수정
    
     JWC-155 #resolve #time 1h

--- client/app/left/team-switch/left.team.switch.directive.js
+++ client/app/left/team-switch/left.team.switch.directive.js
@@ -19,6 +19,8 @@
     };
 
     function link(scope, el, attrs) {
+      var _timer;
+
       _init();
 
       /**
@@ -26,7 +28,9 @@
        * @private
        */
       function _init() {
+        _attachEvent();
         _attachDomEvent();
+        _setMaxHeight();
       }
 
       /**
@@ -35,6 +39,42 @@
        */
       function _attachDomEvent() {
         el.find('._button').on('click', _toggle);
+        $(window).on('resize', _onWindowResize);
+      }
+
+      /**
+       * dom event 바인딩을 해제한다.
+       * @private
+       */
+      function _detachDomEvent() {
+        el.find('._button').off('click', _toggle);
+        $(window).off('resize', _onWindowResize);
+      }
+
+      /**
+       * 이벤트를 바인딩 한다.
+       * @private
+       */
+      function _attachEvent() {
+        scope.$on('$destroy', _onDestroy);
+      }
+
+      /**
+       * max-height 을 설정한다.
+       * @private
+       */
+      function _setMaxHeight() {
+        var height = $('.lpanel-user__user').height() + $('#lpanel-list-container').height() + 17;
+        el.find('._list').css({'max-height': height + 'px'});
+      }
+
+      /**
+       * window resize 시 이벤트 핸들러
+       * @private
+       */
+      function _onWindowResize() {
+        clearTimeout(_timer);
+        _timer = setTimeout(_setMaxHeight, 100);
       }
 
       /**
@@ -43,6 +83,14 @@
        */
       function _toggle() {
         el.find('._list').slideToggle();
+      }
+
+      /**
+       * 소멸자
+       * @private
+       */
+      function _onDestroy() {
+        _detachDomEvent();
       }
     }
   }
