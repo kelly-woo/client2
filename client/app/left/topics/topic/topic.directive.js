9f6365164f326b1e035305ff4a62437c4966cb65
young.park <young.park@tosslab.com>
2016-04-04 17:42:41 +0900

    refactor(left): Left panel 위로 마우스 커서를 계속 움직이며 hover하고 있을 때 CPU 점유율 상승하는 현상 수정
      - 과도한 digest 를 유발하는 로직 제거
        - safeApply 제거
        - ng-이벤트 핸들러를 jquery 이벤트 핸들러로 변경
        - overflow 된 topic name 에 대해 tooltip 노출 하는 로직 최적화
    
    JWC-521 #resolve #time 5h #comment Electron 으로 wrapping 하여 돌려봤을 때 기존 대비 CPU 점유율이 50% 감소되었습니다. (기존 100% -> 현재 50%) QA 시 폴더 상태에 관계 없이 뱃지 카운트 업데이트가 안되는 경우가 발생하는지 확인 부탁드립니다.

--- client/app/left/topics/topic/topic.directive.js
+++ client/app/left/topics/topic/topic.directive.js
@@ -5,7 +5,7 @@
     .module('jandiApp')
     .directive('topic', topic);
 
-  function topic() {
+  function topic(JndUtil) {
     return {
       restrict: 'E',
       controller: 'TopicCtrl',
@@ -17,7 +17,94 @@
       replace: true
     };
 
-    function link(scope, element, attrs) {
+    function link(scope, el, attrs) {
+      var _timer;
+
+      /**
+       * tooltip 이 노출 되었는지 여부
+       * @type {boolean}
+       */
+      scope.isTooltipShown = false;
+
+      _init();
+
+      /**
+       * 초기화 메서드
+       * @private
+       */
+      function _init() {
+        _attachDomEvents();
+      }
+
+      /**
+       * 
+       * @private
+       */
+      function _attachDomEvents() {
+        el.find('._topicName').on('mouseenter', _onMouseEnter)
+          .on('mouseleave', _onMouseLeave);
+      }
+
+      /**
+       * mouse enter 이벤트 핸들러
+       * @param {object} mouseEvent
+       * @private
+       */
+      function _onMouseEnter(mouseEvent) {
+        var jqTarget = $(mouseEvent.target);
+        clearTimeout(_timer);
+        _timer = setTimeout(_.bind(_showTooltip, this, jqTarget), 100);
+      }
+
+      /**
+       * mouse leave 이벤트 핸들러
+       * @param {object} mouseEvent
+       * @private
+       */
+      function _onMouseLeave(mouseEvent) {
+        clearTimeout(_timer);
+        _hideTooltip();
+      }
+
+      /**
+       * topic name 이 overflow 되었는지 여부를 반환한다.
+       * @param {object} jqTarget
+       * @returns {boolean}
+       * @private
+       */
+      function _isTopicNameOverflow(jqTarget) {
+        var jqClone = jqTarget.clone()
+          .css( {display: 'block', width: '100%', visibility: 'hidden'} )
+          .appendTo(jqTarget.parent());
+        var isOverflow = jqClone.width() <= jqTarget.width();
+        jqClone.remove();
+        return isOverflow;
+      }
+
+      /**
+       * 잘린 토픽 이름에 대한 전체 이름 tooltip 을 노출한다.
+       * @param jqTarget
+       * @private
+       */
+      function _showTooltip(jqTarget) {
+        if (!scope.isTooltipShown && _isTopicNameOverflow(jqTarget)) {
+          JndUtil.safeApply(scope, function() {
+            scope.isTooltipShown = true;
+          });
+        }
+      }
+
+      /**
+       * 전체 이름 tooltip 을 감춘다.
+       * @private
+       */
+      function _hideTooltip() {
+        if (scope.isTooltipShown) {
+          JndUtil.safeApply(scope, function() {
+            scope.isTooltipShown = false;
+          });
+        }
+      }
     }
   }
 })();
