c88d4b8ac5314abc8592a78a00348f5deeeba131
tmsla123 <tmsla123@gmail.com>
2016-04-14 12:09:28 +0900

    chore(integration): bak

--- client/components/jandi/ui/intercom/intercom.directive.js
+++ client/components/jandi/ui/intercom/intercom.directive.js
@@ -8,7 +8,7 @@
     .module('jandi.ui.intercom')
     .directive('intercom', intercom);
 
-  function intercom($rootScope, Intercom, memberService) {
+  function intercom($injector, Intercom) {
     return {
       restrict: 'E',
       replace: true,
@@ -19,7 +19,9 @@
       link: link
     };
 
-    function link(scope, el) {
+    function link(scope, el, attr) {
+      var memberService = $injector.get(attr.memberService);
+
       _init();
 
       /**
@@ -30,29 +32,34 @@
         _attachScopeEvents();
       }
 
+      /**
+       * attach scope events
+       * @private
+       */
       function _attachScopeEvents() {
-        scope.$on('headerCtrl:intercomOpen', function() {
-          _boot();
+        scope.$on('onCurrentMemberChanged', _onCurrentMemberChanged);
+        scope.$on('headerCtrl:intercomOpen', _onIntercomOpen);
+      }
+
+      /**
+       * 현재 멤버 정보가 바뀜 이벤트 핸들러
+       */
+      function _onCurrentMemberChanged() {
+        var member = memberService.getMember();
+
+        // intercom에 설정된 멤버정보를 갱신한다.
+        Intercom.update({
+          name: member.name,
+          email: member.u_email
         });
       }
 
-      function _boot() {
-        var member = memberService.getMember();
-
-        //Intercom.show();
-
-        Intercom.shutdown();
-        Intercom.boot({
-          name: member.name,
-          email: member.u_email
-          //widget: {
-          //  activator: '#intercom'
-          //}
-        });
-
-        //window.Intercom('update');
-
-        //window.Intercom('show');
+      /**
+       * intercom 열림 이벤트 핸들러
+       * @private
+       */
+      function _onIntercomOpen() {
+        Intercom.show();
       }
     }
   }
--- client/components/jandi/ui/intercom/intercom.service.js
+++ client/components/jandi/ui/intercom/intercom.service.js
@@ -9,13 +9,10 @@
     .service('Intercom', Intercom);
 
   /* @ngInject */
-  function Intercom($window) {
+  function Intercom($window, configuration) {
     var _that = this;
-    var _options = window.intercomSettings = {
-      app_id: 'yt1d5jat'
-      //widget: {
-      //  activator: '#intercom'
-      //}
+    $window.intercomSettings = {
+      app_id: configuration.intercom_app_id
     };
 
     _init();
@@ -27,24 +24,34 @@
     function _init() {
       _injectIntercomScript();
 
-      _that.boot = boot;
-      _that.shutdown = shutdown;
+      _that.show = show;
+      _that.update = update;
     }
 
+    /**
+     * intercom 스크립트 주입
+     * @private
+     */
     function _injectIntercomScript() {
-      //(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/yt1d5jat';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-
       var intercomScript = "(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/yt1d5jat';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()";
       $('<script>' + intercomScript + '</script>')
         .appendTo($window.document.body);
+      $('<script>window.Intercom("boot", window.intercomSettings); window.Intercom("update");</script>').appendTo($window.document.body);
     }
 
-    function boot(options) {
-      $window.Intercom('boot', _.extend(_options, options));
+    /**
+     * intercom 열림
+     */
+    function show() {
+      $window.Intercom('show');
     }
 
-    function shutdown() {
-      $window.Intercom('shutdown');
+    /**
+     * intercom 정보 갱신
+     * @param setting
+     */
+    function update(setting) {
+      $window.Intercom('update', setting);
     }
   }
 })();
