eda8dbc726f0fd8b8b2824376ec48b0f3a31475c
tmsla123 <tmsla123@gmail.com>
2016-05-11 11:25:39 +0900

    chore(mention): 에러 처리

--- client/app/right/file-detail/comments/comment/file.detail.comment.directive.js
+++ client/app/right/file-detail/comments/comment/file.detail.comment.directive.js
@@ -9,13 +9,14 @@
     .directive('fileDetailComment', fileDetailComment);
 
   /* @ngInject */
-  function fileDetailComment() {
+  function fileDetailComment($compile) {
     return {
       restrict: 'E',
       replace: true,
       scope: {
         content: '='
       },
+      templateUrl : 'app/right/file-detail/comments/comment/file.detail.comment.html',
       link: link
     };
 
@@ -27,9 +28,15 @@
        * @private
        */
       function _init() {
+        var jqContent = $('<div>' + scope.content + '<div>');
+
+        try {
+          $compile(jqContent)(scope);
+        } catch(e) {}
+
         // html을 el.append를 사용하지 않고 ng-bind-html을 사용하여 자식 element로 넣게 되면 mention-view의 data가 날아가
         // 어느 member에 대한 mention인지 구분할 수 없다.
-        el.append(scope.content);
+        el.append(jqContent.html());
       }
     }
   }
